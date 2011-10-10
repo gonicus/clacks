88e9c40dbea4fec7c9d749b61bbeb6c130b40654
Cajus Pollmeier <pollmeier@gonicus.de>
2011-10-10 18:26:36 +0200

    More index kidding

--- contrib/sqlalchemy/index/object_index.py
+++ contrib/sqlalchemy/index/object_index.py
@@ -2,8 +2,9 @@
 # -*- coding: utf-8 -*-
 import uuid
 import datetime
+import sqlalchemy.sql
 from sqlalchemy import create_engine
-from sqlalchemy.sql import select, and_, or_, not_
+from sqlalchemy.sql import select, and_, or_, not_, func
 from sqlalchemy import Table, Column, Integer, Boolean, String, DateTime, Date, Unicode, MetaData, ColumnDefault
 
 
@@ -79,21 +80,91 @@ class IndexEngine(object):
     def update(self, euuid, **props):
         # Convert all list types to Unicode strings
         props = dict([(attr, "|" + "|".join(key) + "|" if type(key) == list else key) for attr, key in props.items()])
-        print props
         self.__conn.execute(self.__index.update().where(self.__index.c._uuid == euuid), [props])
 
+    def count(self, fltr):
+        return self.__conn.execute(
+            select(
+                [func.count(self.__index.c._uuid)],
+                *self.__build_filter(fltr)
+            )).fetchone()[0]
+
+    def search(self, fltr, attrs=None, begin=None, end=None, order_by=None, descending=False):
+        if not attrs:
+            ats = [self.__index]
+        else:
+            ats = []
+            for a in attrs:
+                ats.append(getattr(self.__index.c, a))
+
+        #if begin and end:
+        #    return self.__conn.execute(select(ats, *self.__build_filter(fltr))).fetchall()
+
+        return self.__conn.execute(select(ats, *self.__build_filter(fltr))).fetchall()
+
+    def __build_filter(self, fltr):
+        arg = []
+
+        for el, value in fltr.items():
+
+            if el in ["_and", "_or", "_not"]:
+                if type(value) == dict:
+                    v = getattr(sqlalchemy.sql, el[1:] + "_")(*self.__build_filter(value))
+                    arg.append(v)
+                else:
+                    raise Exception("bad filter")
+
+            elif el in ["_gt", "_lt", "_ge", "_le"]:
+                if len(value) != 2:
+                    raise Exception("bad filter")
+
+                if type(value) == list and len(value) == 2:
+                    if el == "_gt":
+                        arg.append(getattr(self.__index.c, value[0]) > value[1])
+                    if el == "_lt":
+                        arg.append(getattr(self.__index.c, value[0]) < value[1])
+                    if el == "_ge":
+                        arg.append(getattr(self.__index.c, value[0]) >= value[1])
+                    if el == "_le":
+                        arg.append(getattr(self.__index.c, value[0]) <= value[1])
+                else:
+                    raise Exception("bad filter")
+
+            elif el == "_in":
+                if len(value) != 1:
+                    raise Exception("bad filter")
+
+                k, v = value.items()[0]
+                if type(v) != list:
+                    raise Exception("bad filter")
+
+                arg.append(getattr(self.__index.c, k).in_(v))
+
+            else:
+                arg.append(getattr(self.__index.c, el.split("_")[0]) == value)
+
+        return arg
+
+#       tmp = self.__conn.execute(select([self.__index], self.__index.c._uuid == euuid))
+#       self.__conn.execute()
 #---------------------------------------------------------------------
 
-ie = IndexEngine({'givenName': Unicode, 'sn': Unicode, 'mail': String, 'uid': String})
+ie = IndexEngine({'givenName': Unicode, 'sn': Unicode, 'mail': String, 'uid': String, 'dob': Date})
 #ie = IndexEngine({'givenName': Unicode, 'sn': Unicode, 'mail': String, 'uid': String, 'uidNumber': Integer})
-
-#ie.count(filter)
-#ie.search(filter, begin, end)
 
 u1 = str(uuid.uuid4())
 ie.insert(u1, dn=u"cn=Cajus Pollmeier,ou=people,ou=Technik,dc=gonicus,dc=de", sn=u"Pollmeier", givenName=u"Cajus", uid="cajus", mail=['cajus@debian.org', 'cajus@naasa.net', 'pollmeier@gonicus.de'], _lastChanged=datetime.datetime.now())
+
 print ie.has_entry(u1)
 ie.update(u1, uid="lorenz")
+
+
+fltr = {'uid': 'lorenz'}
+#fltr = {'_and': {'uid': 'lorenz', 'givenName': u'Cajus', '_or': {'sn': u'ding', 'sn_2': u'dong', '_gt': ['dob', datetime.datetime.now()]}}}
+#fltr = {'_and': {'uid': 'lorenz', 'givenName': u'Cajus', '_or': {'_in': {'sn': [u'ding', u'dong']}, '_gt': ['dob', datetime.datetime.now()]}}}
+print "Count:", ie.count(fltr)
+print ie.search(fltr, attrs=['sn', 'givenName', 'uid'], begin=0, end=10)
+
 ie.remove(u1)
 print ie.has_entry(u1)
 
