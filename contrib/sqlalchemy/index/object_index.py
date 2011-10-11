bf2163220c565a1a05514d242ad2f495cca740fc
Cajus Pollmeier <pollmeier@gonicus.de>
2011-10-11 11:20:24 +0200

    Updated object indexing test

--- contrib/sqlalchemy/index/object_index.py
+++ contrib/sqlalchemy/index/object_index.py
@@ -14,13 +14,24 @@ class FilterException(Exception):
 
 
 class IndexEngine(object):
-
+    __type_conv = {
+            'Integer': Integer,
+            'Boolean': Boolean,
+            'String': String,
+            'UnicodeString': Unicode,
+            'Date': Date,
+            'Timestamp': DateTime,
+            }
+    __types = None
     __engine = None
     __conn = None
     __fixed = ['id', '_dn', '_uuid', '_lastChanged', '_extensions']
 
     def __init__(self, index_attrs):
+        self.__types = index_attrs
         self.__engine = create_engine('sqlite:///index.db')
+
+        #TODO: schedule the following code in the
 
         # Table name
         idx = "index"
@@ -31,7 +42,7 @@ class IndexEngine(object):
 
         reset = True
         if idx in meta.tables:
-            current_attrs = set(index_attrs.keys())
+            current_attrs = set(self.__types.keys())
             db_attrs = set([str(x)[len(idx) + 1:] for x in meta.tables[idx]._columns if not str(x)[len(idx) + 1:] in self.__fixed])
 
             if current_attrs == db_attrs:
@@ -41,15 +52,21 @@ class IndexEngine(object):
                     # Check if we at least subclass the type we got from the
                     # database
                     s_type = type(getattr(meta.tables[idx]._columns, attr).type)
-                    if (not issubclass(s_type, index_attrs[attr])):
+                    try:
+                        d_type = self.__type_conv[self.__types[attr]['type']]
+                    except KeyError:
+                        print "---> type not supported for indexing:", self.__types[attr]['type']
+                        continue
+
+                    if (not issubclass(s_type, d_type)):
 
                         # If that fails, maybe there's one other try in the
                         # class hierachy
-                        superiours = inspect.getmro(index_attrs[attr])
+                        superiours = inspect.getmro(d_type)
                         if not (len(superiours) > 1 and issubclass(s_type, superiours[1])):
                             print "Looks like we've to refresh the object index:"
                             print "I-->", s_type
-                            print "S-->", index_attrs[attr]
+                            print "S-->", d_type
                             break
                 else:
                     reset = False
@@ -62,8 +79,13 @@ class IndexEngine(object):
         metadata = MetaData()
 
         props = []
-        for attr, typ in index_attrs.items():
-            props.append(Column(attr, typ))
+        for attr, info in self.__types.items():
+            try:
+                d_type = self.__type_conv[self.__types[attr]['type']]
+            except KeyError:
+                print "---> type not supported for indexing:", self.__types[attr]['type']
+                continue
+            props.append(Column(attr, d_type))
 
         self.__index = Table(idx, metadata,
             Column('_uuid', String, primary_key=True),
@@ -107,6 +129,7 @@ class IndexEngine(object):
             )).fetchone()[0]
 
     def search(self, fltr, attrs=None, begin=None, end=None, order_by=None, descending=False):
+
         if not attrs:
             ats = [self.__index]
         else:
@@ -136,6 +159,7 @@ class IndexEngine(object):
         arg = []
 
         for el, value in fltr.items():
+            cel = el.split("_")[0]
 
             if el in ["_and", "_or", "_not"]:
                 if type(value) == dict:
@@ -170,29 +194,44 @@ class IndexEngine(object):
 
                 arg.append(getattr(self.__index.c, k).in_(v))
 
+            elif not cel in self.__types:
+                raise FilterException("attribute '%s' is not indexed" % el.split("_")[0])
+
+            elif self.__types[cel]['multi']:
+                value = value.replace("|", r"\|")
+
+                if '%' in value:
+                    value = value.replace('%', '[^|]*')
+
+                arg.append(getattr(self.__index.c, cel).op('regexp')(r"|%s|" % value))
+
             else:
-                arg.append(getattr(self.__index.c, el.split("_")[0]) == value)
+                if '%' in value:
+                    arg.append(getattr(self.__index.c, cel).like(value))
+                else:
+                    arg.append(getattr(self.__index.c, cel) == value)
 
         return arg
 
-#       tmp = self.__conn.execute(select([self.__index], self.__index.c._uuid == euuid))
-#       self.__conn.execute()
 #---------------------------------------------------------------------
 
-ie = IndexEngine({'givenName': Unicode, 'sn': Unicode, 'mail': String, 'uid': String, 'dob': Date})
-#ie = IndexEngine({'givenName': Unicode, 'sn': Unicode, 'mail': String, 'uid': String, 'uidNumber': Integer})
+ie = IndexEngine({
+    'givenName': {'type': 'UnicodeString', 'multi': False},
+    'sn': {'type': 'UnicodeString', 'multi': False},
+    'mail': {'type': 'String', 'multi': True},
+    'uid': {'type': 'String', 'multi': False},
+    'uidNumber': {'type': 'Integer', 'multi': False},
+    'dateOfBirth': {'type': 'Date', 'multi': False}})
 
-u1 = str(uuid.uuid4())
-ie.insert(u1, dn=u"cn=Cajus Pollmeier,ou=people,ou=Technik,dc=gonicus,dc=de", sn=u"Pollmeier", givenName=u"Cajus", uid="cajus", mail=['cajus@debian.org', 'cajus@naasa.net', 'pollmeier@gonicus.de'], _lastChanged=datetime.datetime.now())
+#u1 = str(uuid.uuid4())
+#ie.insert(u1, dn=u"cn=Cajus Pollmeier,ou=people,ou=Technik,dc=gonicus,dc=de", sn=u"Pollmeier", givenName=u"Cajus", uid="cajus", mail=['cajus@debian.org', 'cajus@naasa.net', 'pollmeier@gonicus.de'], _lastChanged=datetime.datetime.now())
+#print ie.has_entry(u1)
+#ie.update(u1, uid="lorenz")
 
-print ie.has_entry(u1)
-ie.update(u1, uid="lorenz")
-
-
-#TODO: wildcard, maybe regex?
 #TODO: filter for list elements -> |....|....|....|
 #fltr = {}
-fltr = {'uid': 'lorenz'}
+#fltr = {'uid': 'lo%'}
+fltr = {'mail': 'cajus@%'}
 #fltr = {'_and': {'uid': 'lorenz', 'givenName': u'Cajus', '_or': {'sn': u'ding', 'sn_2': u'dong', '_gt': ['dob', datetime.datetime.now()]}}}
 #fltr = {'_and': {'uid': 'lorenz', 'givenName': u'Cajus', '_or': {'_in': {'sn': [u'ding', u'dong']}, '_gt': ['dob', datetime.datetime.now()]}}}
 print "Count:", ie.count(fltr)
