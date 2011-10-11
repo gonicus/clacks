e69d39fe275ae7969ce69971bc540b8e302972ba
Cajus Pollmeier <pollmeier@gonicus.de>
2011-10-11 09:10:02 +0200

    Updated index test

--- contrib/sqlalchemy/index/object_index.py
+++ contrib/sqlalchemy/index/object_index.py
@@ -2,16 +2,22 @@
 # -*- coding: utf-8 -*-
 import uuid
 import datetime
+import inspect
 import sqlalchemy.sql
 from sqlalchemy import create_engine
-from sqlalchemy.sql import select, and_, or_, not_, func
+from sqlalchemy.sql import select, and_, or_, not_, func, asc
 from sqlalchemy import Table, Column, Integer, Boolean, String, DateTime, Date, Unicode, MetaData, ColumnDefault
+
+
+class FilterException(Exception):
+    pass
 
 
 class IndexEngine(object):
 
     __engine = None
     __conn = None
+    __fixed = ['id', '_dn', '_uuid', '_lastChanged', '_extensions']
 
     def __init__(self, index_attrs):
         self.__engine = create_engine('sqlite:///index.db')
@@ -26,15 +32,25 @@ class IndexEngine(object):
         reset = True
         if idx in meta.tables:
             current_attrs = set(index_attrs.keys())
-            db_attrs = set([str(x)[len(idx) + 1:] for x in meta.tables[idx]._columns if not str(x)[len(idx) + 1:] in ['id', '_dn', '_uuid', '_lastChanged', ]])
+            db_attrs = set([str(x)[len(idx) + 1:] for x in meta.tables[idx]._columns if not str(x)[len(idx) + 1:] in self.__fixed])
 
             if current_attrs == db_attrs:
 
                 for attr in db_attrs:
-                    if (getattr(meta.tables[idx]._columns, attr).type) != index_attrs[attr]:
-                        #TODO: this is fuzzy because VARCHAR != STRING, etc.
-                        print "FIXME"
-                        break
+
+                    # Check if we at least subclass the type we got from the
+                    # database
+                    s_type = type(getattr(meta.tables[idx]._columns, attr).type)
+                    if (not issubclass(s_type, index_attrs[attr])):
+
+                        # If that fails, maybe there's one other try in the
+                        # class hierachy
+                        superiours = inspect.getmro(index_attrs[attr])
+                        if not (len(superiours) > 1 and issubclass(s_type, superiours[1])):
+                            print "Looks like we've to refresh the object index:"
+                            print "I-->", s_type
+                            print "S-->", index_attrs[attr]
+                            break
                 else:
                     reset = False
 
@@ -53,6 +69,7 @@ class IndexEngine(object):
             Column('_uuid', String, primary_key=True),
             Column('_dn', Unicode),
             Column('_lastChanged', DateTime),
+            Column('_extensions', String),
             *props)
 
         metadata.create_all(self.__engine)
@@ -97,10 +114,23 @@ class IndexEngine(object):
             for a in attrs:
                 ats.append(getattr(self.__index.c, a))
 
-        #if begin and end:
-        #    return self.__conn.execute(select(ats, *self.__build_filter(fltr))).fetchall()
+        sl = select(ats, *self.__build_filter(fltr))
 
-        return self.__conn.execute(select(ats, *self.__build_filter(fltr))).fetchall()
+        # Apply ordering
+        if order_by:
+            if descending:
+                sl = sl.order_by(order_by)
+            else:
+                sl = sl.order_by(asc(order_by))
+
+        # Apply range
+        if begin != None and end != None:
+            if begin >= end:
+                raise FilterException("filter range error - 'begin' is not < 'end'")
+
+            sl = sl.offset(begin).limit(end - begin + 1)
+
+        return self.__conn.execute(sl).fetchall()
 
     def __build_filter(self, fltr):
         arg = []
@@ -112,11 +142,11 @@ class IndexEngine(object):
                     v = getattr(sqlalchemy.sql, el[1:] + "_")(*self.__build_filter(value))
                     arg.append(v)
                 else:
-                    raise Exception("bad filter")
+                    raise FilterException("operator needs a dict argument")
 
             elif el in ["_gt", "_lt", "_ge", "_le"]:
                 if len(value) != 2:
-                    raise Exception("bad filter")
+                    raise FilterException("operator needs a list with exactly two parameters")
 
                 if type(value) == list and len(value) == 2:
                     if el == "_gt":
@@ -128,15 +158,15 @@ class IndexEngine(object):
                     if el == "_le":
                         arg.append(getattr(self.__index.c, value[0]) <= value[1])
                 else:
-                    raise Exception("bad filter")
+                    raise FilterException("operator needs a list with exactly two parameters")
 
             elif el == "_in":
                 if len(value) != 1:
-                    raise Exception("bad filter")
+                    raise FilterException("operator expects a single list argument")
 
                 k, v = value.items()[0]
                 if type(v) != list:
-                    raise Exception("bad filter")
+                    raise FilterException("operator expects a single list argument")
 
                 arg.append(getattr(self.__index.c, k).in_(v))
 
@@ -159,13 +189,17 @@ print ie.has_entry(u1)
 ie.update(u1, uid="lorenz")
 
 
+#TODO: wildcard, maybe regex?
+#TODO: filter for list elements -> |....|....|....|
+#fltr = {}
 fltr = {'uid': 'lorenz'}
 #fltr = {'_and': {'uid': 'lorenz', 'givenName': u'Cajus', '_or': {'sn': u'ding', 'sn_2': u'dong', '_gt': ['dob', datetime.datetime.now()]}}}
 #fltr = {'_and': {'uid': 'lorenz', 'givenName': u'Cajus', '_or': {'_in': {'sn': [u'ding', u'dong']}, '_gt': ['dob', datetime.datetime.now()]}}}
 print "Count:", ie.count(fltr)
-print ie.search(fltr, attrs=['sn', 'givenName', 'uid'], begin=0, end=10)
+for e in ie.search(fltr, attrs=['sn', 'givenName', 'uid'], begin=0, end=10):
+    print e
 
-ie.remove(u1)
-print ie.has_entry(u1)
+#ie.remove(u1)
+#print ie.has_entry(u1)
 
 #ie.refresh()
