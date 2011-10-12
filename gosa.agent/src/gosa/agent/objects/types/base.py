2755bcc8d77a538cb8edab05da30735661f54202
Fabian Hickert <fabian.hickert@gonicus.de>
2011-10-12 07:17:24 +0200

    Updated object-types defintions

--- gosa.agent/src/gosa/agent/objects/types/base.py
+++ gosa.agent/src/gosa/agent/objects/types/base.py
@@ -5,110 +5,110 @@ import datetime
 class StringAttribute(AttributeType):
     __alias__ = "String"
 
-    def _convert_to_string(cls, value):
+    def _convert_to_string(self, value):
         return(value)
 
-    def _convert_from_string(cls, value):
+    def _convert_from_string(self, value):
         return(value)
 
-    def is_valid_value(cls, value):
+    def is_valid_value(self, value):
         return(not len(value) or all(map(lambda x: type(x) == str, value)))
 
-    def values_match(cls, value1, value2):
+    def values_match(self, value1, value2):
         return(value1 == value2)
 
 
 class IntegerAttribute(AttributeType):
     __alias__ = "Integer"
 
-    def _convert_to_integer(cls, value):
+    def _convert_to_integer(self, value):
         return(value)
 
-    def _convert_from_integer(cls, value):
+    def _convert_from_integer(self, value):
         return(value)
 
-    def is_valid_value(cls, value):
+    def is_valid_value(self, value):
         return(not len(value) or all(map(lambda x: type(x) == int, value)))
 
-    def values_match(cls, value1, value2):
+    def values_match(self, value1, value2):
         return(value1 == value2)
 
 
 class BooleanAttribute(AttributeType):
     __alias__ = "Boolean"
 
-    def is_valid_value(cls, value):
+    def is_valid_value(self, value):
         return(not len(value) or all(map(lambda x: type(x) == bool, value)))
 
-    def _convert_to_boolean(cls, value):
+    def _convert_to_boolean(self, value):
         return(value)
 
-    def _convert_from_boolean(cls, value):
+    def _convert_from_boolean(self, value):
         return(value)
 
-    def values_match(cls, value1, value2):
+    def values_match(self, value1, value2):
         return(value1 == value2)
 
 
 class BinaryAttribute(AttributeType):
     __alias__ = "Binary"
 
-    def _convert_to_binary(cls, value):
+    def _convert_to_binary(self, value):
         return(value)
 
-    def _convert_from_binary(cls, value):
+    def _convert_from_binary(self, value):
         return(value)
 
-    def is_valid_value(cls, value):
+    def is_valid_value(self, value):
         return(not len(value) or all(map(lambda x: type(x) == unicode, value)))
 
-    def values_match(cls, value1, value2):
+    def values_match(self, value1, value2):
         return(value1 == value2)
 
 
 class UnicodeStringAttribute(AttributeType):
     __alias__ = "UnicodeString"
 
-    def _convert_to_unicodestring(cls, value):
+    def _convert_to_unicodestring(self, value):
         return(value)
 
-    def _convert_from_unicodestring(cls, value):
+    def _convert_from_unicodestring(self, value):
         return(value)
 
-    def is_valid_value(cls, value):
+    def is_valid_value(self, value):
         return(not len(value) or all(map(lambda x: type(x) == unicode, value)))
 
-    def values_match(cls, value1, value2):
+    def values_match(self, value1, value2):
         return(value1 == value2)
 
 
 class DateAttribute(AttributeType):
     __alias__ = "Date"
 
-    def _convert_to_date(cls, value):
+    def _convert_to_date(self, value):
         return(value)
 
-    def _convert_from_date(cls, value):
+    def _convert_from_date(self, value):
         return(value)
 
-    def is_valid_value(cls, value):
+    def is_valid_value(self, value):
         return(not len(value) or all(map(lambda x: type(x) == datetime.date, value)))
 
-    def values_match(cls, value1, value2):
+    def values_match(self, value1, value2):
         return(value1 == value2)
 
 
 class TimestampAttribute(AttributeType):
     __alias__ = "Timestamp"
 
-    def _convert_to_timestamp(cls, value):
+    def _convert_to_timestamp(self, value):
         return(value)
 
-    def _convert_from_timestamp(cls, value):
+    def _convert_from_timestamp(self, value):
         return(value)
 
-    def is_valid_value(cls, value):
+    def is_valid_value(self, value):
         return(not len(value) or all(map(lambda x: type(x) == datetime.datetime, value)))
 
-    def values_match(cls, value1, value2):
+    def values_match(self, value1, value2):
         return(value1 == value2)
