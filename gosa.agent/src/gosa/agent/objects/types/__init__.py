1d5ac8a33279bb5c5f3eb6bc4a4bdad7edd48c44
Fabian Hickert <fabian.hickert@gonicus.de>
2011-10-12 07:15:50 +0200

    Removed @classmethod decorator

--- gosa.agent/src/gosa/agent/objects/types/__init__.py
+++ gosa.agent/src/gosa/agent/objects/types/__init__.py
@@ -8,7 +8,6 @@ class ConversationNotSupported(Exception):
 
 class AttributeType(object):
 
-    @classmethod
     def _cnv_topic(cls):
         fname = inspect.stack()[1][3]
         if fname[12::] == "_convert_to_":
@@ -16,76 +15,58 @@ class AttributeType(object):
         else:
             return (cls.__alias__.lower(), fname[14:].replace("_", " "))
 
-    @classmethod
     def is_valid_value(cls, value):
         raise ConversationNotSupported("Cannot check value type for '%s'!" % (cls.__alias__.lower(),))
 
-    @classmethod
     def values_match(cls, value1, value2):
         raise ConversationNotSupported("Value check not implemented for '%s'!" % (cls.__alias__.lower(),))
 
-    @classmethod
     def convert_to(cls, target_type, value):
         cnv = getattr(cls, "_convert_to_%s" % target_type.lower())
         return cnv(value)
 
-    @classmethod
     def convert_from(cls, source_type, value):
         cnv = getattr(cls, "_convert_from_%s" % source_type.lower())
         return cnv(value)
 
-    @classmethod
     def _convert_to_boolean(cls, value):
         raise ConversationNotSupported("Cannot convert from '%s' to '%s'" % cls._cnv_topic())
 
-    @classmethod
     def _convert_to_string(cls, value):
         raise ConversationNotSupported("Cannot convert from '%s' to '%s'" % cls._cnv_topic())
 
-    @classmethod
     def _convert_to_unicodestring(cls, value):
         raise ConversationNotSupported("Cannot convert from '%s' to '%s'" % cls._cnv_topic())
 
-    @classmethod
     def _convert_to_integer(cls, value):
         raise ConversationNotSupported("Cannot convert from '%s' to '%s'" % cls._cnv_topic())
 
-    @classmethod
     def _convert_to_timestamp(cls, value):
         raise ConversationNotSupported("Cannot convert from '%s' to '%s'" % cls._cnv_topic())
 
-    @classmethod
     def _convert_to_date(cls, value):
         raise ConversationNotSupported("Cannot convert from '%s' to '%s'" % cls._cnv_topic())
 
-    @classmethod
     def _convert_to_binary(cls, value):
         raise ConversationNotSupported("Cannot convert from '%s' to '%s'" % cls._cnv_topic())
 
-    @classmethod
     def _convert_from_boolean(cls, value):
         raise ConversationNotSupported("Cannot convert to '%s' from '%s'" % cls._cnv_topic())
 
-    @classmethod
     def _convert_from_string(cls, value):
         raise ConversationNotSupported("Cannot convert to '%s' from '%s'" % cls._cnv_topic())
 
-    @classmethod
     def _convert_from_unicodestring(cls, value):
         raise ConversationNotSupported("Cannot convert to '%s' from '%s'" % cls._cnv_topic())
 
-    @classmethod
     def _convert_from_integer(cls, value):
         raise ConversationNotSupported("Cannot convert to '%s' from '%s'" % cls._cnv_topic())
 
-    @classmethod
     def _convert_from_timestamp(cls, value):
         raise ConversationNotSupported("Cannot convert to '%s' from '%s'" % cls._cnv_topic())
 
-    @classmethod
     def _convert_from_date(cls, value):
         raise ConversationNotSupported("Cannot convert to '%s' from '%s'" % cls._cnv_topic())
 
-    @classmethod
     def _convert_from_binary(cls, value):
         raise ConversationNotSupported("Cannot convert to '%s' from '%s'" % cls._cnv_topic())
--- gosa.agent/src/gosa/agent/objects/types/base.py
+++ gosa.agent/src/gosa/agent/objects/types/base.py
@@ -5,19 +5,15 @@ import datetime
 class StringAttribute(AttributeType):
     __alias__ = "String"
 
-    @classmethod
     def _convert_to_string(cls, value):
         return(value)
 
-    @classmethod
     def _convert_from_string(cls, value):
         return(value)
 
-    @classmethod
     def is_valid_value(cls, value):
         return(not len(value) or all(map(lambda x: type(x) == str, value)))
 
-    @classmethod
     def values_match(cls, value1, value2):
         return(value1 == value2)
 
@@ -25,19 +21,15 @@ class StringAttribute(AttributeType):
 class IntegerAttribute(AttributeType):
     __alias__ = "Integer"
 
-    @classmethod
     def _convert_to_integer(cls, value):
         return(value)
 
-    @classmethod
     def _convert_from_integer(cls, value):
         return(value)
 
-    @classmethod
     def is_valid_value(cls, value):
         return(not len(value) or all(map(lambda x: type(x) == int, value)))
 
-    @classmethod
     def values_match(cls, value1, value2):
         return(value1 == value2)
 
@@ -45,19 +37,15 @@ class IntegerAttribute(AttributeType):
 class BooleanAttribute(AttributeType):
     __alias__ = "Boolean"
 
-    @classmethod
     def is_valid_value(cls, value):
         return(not len(value) or all(map(lambda x: type(x) == bool, value)))
 
-    @classmethod
     def _convert_to_boolean(cls, value):
         return(value)
 
-    @classmethod
     def _convert_from_boolean(cls, value):
         return(value)
 
-    @classmethod
     def values_match(cls, value1, value2):
         return(value1 == value2)
 
@@ -65,19 +53,15 @@ class BooleanAttribute(AttributeType):
 class BinaryAttribute(AttributeType):
     __alias__ = "Binary"
 
-    @classmethod
     def _convert_to_binary(cls, value):
         return(value)
 
-    @classmethod
     def _convert_from_binary(cls, value):
         return(value)
 
-    @classmethod
     def is_valid_value(cls, value):
         return(not len(value) or all(map(lambda x: type(x) == unicode, value)))
 
-    @classmethod
     def values_match(cls, value1, value2):
         return(value1 == value2)
 
@@ -85,19 +69,15 @@ class BinaryAttribute(AttributeType):
 class UnicodeStringAttribute(AttributeType):
     __alias__ = "UnicodeString"
 
-    @classmethod
     def _convert_to_unicodestring(cls, value):
         return(value)
 
-    @classmethod
     def _convert_from_unicodestring(cls, value):
         return(value)
 
-    @classmethod
     def is_valid_value(cls, value):
         return(not len(value) or all(map(lambda x: type(x) == unicode, value)))
 
-    @classmethod
     def values_match(cls, value1, value2):
         return(value1 == value2)
 
@@ -105,19 +85,15 @@ class UnicodeStringAttribute(AttributeType):
 class DateAttribute(AttributeType):
     __alias__ = "Date"
 
-    @classmethod
     def _convert_to_date(cls, value):
         return(value)
 
-    @classmethod
     def _convert_from_date(cls, value):
         return(value)
 
-    @classmethod
     def is_valid_value(cls, value):
         return(not len(value) or all(map(lambda x: type(x) == datetime.date, value)))
 
-    @classmethod
     def values_match(cls, value1, value2):
         return(value1 == value2)
 
@@ -125,18 +101,14 @@ class DateAttribute(AttributeType):
 class TimestampAttribute(AttributeType):
     __alias__ = "Timestamp"
 
-    @classmethod
     def _convert_to_timestamp(cls, value):
         return(value)
 
-    @classmethod
     def _convert_from_timestamp(cls, value):
         return(value)
 
-    @classmethod
     def is_valid_value(cls, value):
         return(not len(value) or all(map(lambda x: type(x) == datetime.datetime, value)))
 
-    @classmethod
     def values_match(cls, value1, value2):
         return(value1 == value2)
