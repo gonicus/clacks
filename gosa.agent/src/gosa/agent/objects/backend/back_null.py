2e051c10d577c6dd7059b73d8e32169ed8ef225f
Cajus Pollmeier <pollmeier@gonicus.de>
2011-10-12 12:17:25 +0200

    Added null backend

---
+++ gosa.agent/src/gosa/agent/objects/backend/back_null.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from gosa.agent.objects.backend import ObjectBackend
+
+
+class NULL(ObjectBackend):
+
+    def __init__(self):
+        pass
+
+    def load(self, uuid, info):
+        return {}
+
+    def identify(self, dn, params):
+        return False
+
+    def exists(self, misc):
+        return False
+
+    def remove(self, uuid, recursive=False):
+        return True
+
+    def retract(self, uuid, data, params):
+        pass
+
+    def extend(self, uuid, data, params, foreign_keys):
+        return None
+
+    def move_extension(self, uuid, new_base):
+        pass
+
+    def move(self, uuid, new_base):
+        return True
+
+    def create(self, base, data, params, foreign_keys=None):
+        return None
+
+    def update(self, uuid, data):
+        return True
+
+    def is_uniq(self, attr, value, at_type):
+        return False
