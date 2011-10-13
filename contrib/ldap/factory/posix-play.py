56afafdd6d671ffcf4bb4365a10d967c3cd4e260
Fabian Hickert <fabian.hickert@gonicus.de>
2011-10-13 16:53:27 +0200

    Added posix playground

---
+++ contrib/ldap/factory/posix-play.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import time
+import datetime
+import sys
+import os
+from pprint import pprint
+from gosa.agent.objects import GOsaObjectFactory
+
+
+f = GOsaObjectFactory()
+
+## Update
+object_type, extensions =  f.identifyObject(u'cn=Playground Tester,ou=people,dc=gonicus,dc=de')
+
+# Add or remove samba
+if not 'PosixUser' in extensions:
+    p = f.getObject('PosixUser', u'cn=Playground Tester,ou=people,dc=gonicus,dc=de', mode='extend')
+    p.homeDirectory = "\home\hickert"
+    p.commit()
+    print "Extending!"
+
+else:
+    p = f.getObject('PosixUser', u'cn=Playground Tester,ou=people,dc=gonicus,dc=de')
+    p.retract()
+    print "Retracting!"
+
+#p = f.getObject('PosixUser', u'cn=Playground Tester,ou=people,dc=gonicus,dc=de', mode='update')
+#for entry in p.listProperties():
+#    print "%30s" % (entry,), getattr(p, entry)
+
+p = f.getObject('PosixUser', u'cn=Playground Tester,ou=people,dc=gonicus,dc=de', mode='update')
+p.commit()
