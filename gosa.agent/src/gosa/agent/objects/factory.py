51a0b6bbad615d543d55d37716c5c6d3608e9ca3
Fabian Hickert <fabian.hickert@gonicus.de>
2011-10-14 11:04:11 +0200

    Updated groups xml

--- gosa.agent/src/gosa/agent/objects/factory.py
+++ gosa.agent/src/gosa/agent/objects/factory.py
@@ -1042,6 +1042,10 @@ class GOsaObject(object):
 
             # Set the new value
             if props[name]['multivalue']:
+
+                # Check if the new value is s list.
+                if not type(value) == list:
+                    raise TypeError("Invalid value given for %s, expected is a list for multi value fields!" % (name,))
                 new_value = value
             else:
                 new_value = [value]
