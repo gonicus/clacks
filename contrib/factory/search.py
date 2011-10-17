9c134827934b600a8552395e6015ea380e85464f
Cajus Pollmeier <pollmeier@gonicus.de>
2011-10-17 11:03:04 +0200

    Updated search test to be more variable

--- contrib/factory/search.py
+++ contrib/factory/search.py
@@ -1,30 +1,62 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import json
+import sys
+import argparse
 from gosa.agent.objects import GOsaObjectFactory
 from gosa.agent.objects.index import ObjectIndex, SCOPE_ONE, SCOPE_BASE, SCOPE_SUB
 
+# Define cla parser
+parser = argparse.ArgumentParser(description='Search test')
+parser.add_argument('query', metavar='QUERY', type=unicode, help='JSON query', nargs=1)
+parser.add_argument('attrs', metavar='ATTRIBUTE', type=str, help='query attributes', nargs="*")
+parser.add_argument('--sync', dest='sync', action='store_true', default=False, help='sync index')
+parser.add_argument('--base', dest='base', type=unicode, default="dc=gonicus,dc=de", help='the search base')
+parser.add_argument('--scope', dest='scope', default="sub", help='the search scope')
+parser.add_argument('--offset', dest='offset', type=int, default=None, help='the search scope')
+parser.add_argument('--count', dest='count', type=int, default=None, help='the search scope')
+args = parser.parse_args()
+
+# Load filter
+try:
+    fltr = json.loads(args.query[0])
+except:
+    print "Error: invalid filter\n"
+    print "Examples:"
+    print '  {"uid": "*us"}'
+    print '  {"mail": "stefan.grote@*"}'
+    print '  {"_and": {"uid": "lorenz", "givenName": u"Cajus", "_or": {"_in": {"sn": [u"ding", u"dong"]}}}}'
+    exit(1)
+
+# Load scope
+scope = None
+if args.scope == "sub":
+    scope = SCOPE_SUB
+if args.scope == "one":
+    scope = SCOPE_ONE
+if args.scope == "base":
+    scope = SCOPE_BASE
+if not scope:
+    print "Error: invalid scope"
+    exit(1)
+
+sys.argv = [sys.argv[0]]
 ie = ObjectIndex()
 
 # Test synchronisation
-if 1 == 1:
-    print "-"*80 + "\nSync\n" + "-"*80
+if args.sync:
+    print "Please wait - updating index..."
     ie.sync_index()
-    print "-"*80 + "\nSearch\n" + "-"*80
 
-fltr = {}
-#fltr = {'uid': '*us'}
-#fltr = {'mail': 'stefan.grote@*'}
-#fltr = {'_and': {'uid': 'lorenz', 'givenName': u'Cajus', '_or': {'sn': u'ding', 'sn_2': u'dong', '_gt': ['dob', datetime.datetime.now()]}}}
-#fltr = {'_and': {'uid': 'lorenz', 'givenName': u'Cajus', '_or': {'_in': {'sn': [u'ding', u'dong']}, '_gt': ['dob', datetime.datetime.now()]}}}
+start = stop = None
+if args.offset and args.count:
+    start = args.offset
+    stop = args.ofset + args.count - 1
 
-# Deliver a count for a specific base
-print "Count:", ie.count(base="dc=gonicus,dc=de", fltr=fltr)
-for e in ie.search(base=u"dc=gonicus,dc=de", scope=SCOPE_SUB, fltr=fltr,
-        attrs=['sn', 'givenName', 'uid', 'mail', '_dn'], begin=0, end=10):
+for e in ie.search(base=args.base, scope=scope, fltr=fltr, attrs=args.attrs,
+        begin=start, end=stop):
     print e
+exit(0)
 
-print "-"*80 + "\nEvent\n" + "-"*80
-f = GOsaObjectFactory.getInstance()
-p = f.getObject('GenericUser', u'cn=Klaus Mustermann,ou=people,dc=gonicus,dc=de')
-p.mail = ['munter@brechen.de']
-p.commit()
+
+print "\nCount:", ie.count(base="dc=gonicus,dc=de", fltr=fltr)
