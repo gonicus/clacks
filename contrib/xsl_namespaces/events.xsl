5e8c8afa90067ec5ebed897789859438c6c4f9f6
Fabian Hickert <fabian.hickert@gonicus.de>
2011-11-08 15:59:53 +0100

    Added XSL Namespace test file.

---
+++ contrib/xsl_namespaces/events.xsl
@@ -0,0 +1,50 @@
+<?xml version="1.0" encoding="UTF-8"?>
+
+<xsl:stylesheet version="1.0"
+	xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:gosa="http://www.gonicus.de/Events">
+
+	<xsl:output method="xml" indent="yes" encoding="UTF-8" />
+
+	<xsl:template match="/">
+
+		<schema targetNamespace="http://www.gonicus.de/Events"
+			elementFormDefault="qualified" 
+			xmlns="http://www.w3.org/2001/XMLSchema"
+			xmlns:xs="http://www.w3.org/2001/XMLSchema">
+
+			<!-- Include schema for each event -->
+			<xsl:for-each select="/events/path">
+				<xsl:variable name="nodename">
+					<xsl:value-of select="@name" />
+				</xsl:variable>
+				<import schemaLocation="{text()}"
+					namespace="http://www.gonicus.de/Events/{@name}"/> 
+			</xsl:for-each>
+
+			<complexType name="Event">
+				<choice maxOccurs="1" minOccurs="1">
+					<group ref="gosa:Events"></group>
+				</choice>
+			</complexType>
+
+			<group name="Events">
+				<choice>
+					<!-- Create the possible Events -->
+					<xsl:for-each select="/events/path">
+						<xsl:variable name="nodename">
+							<xsl:value-of select="@name" />
+						</xsl:variable>
+
+						<xsl:element name="xs:element">
+							<xsl:attribute name="xmlns">http://www.gonicus.de/Events/<xsl:value-of select="@name"
+									/></xsl:attribute>
+							<xsl:attribute name="type"><xsl:value-of select="@name"/></xsl:attribute>
+							<xsl:attribute name="name"><xsl:value-of select="@name"/></xsl:attribute>
+						</xsl:element>
+					</xsl:for-each>
+				</choice>
+			</group>
+			<element name="Event" type="gosa:Event"></element>
+		</schema>
+	</xsl:template>
+</xsl:stylesheet>
---
+++ contrib/xsl_namespaces/test.py
@@ -0,0 +1,61 @@
+import StringIO
+from lxml import etree, objectify
+import os
+
+# Schema files to combine
+xsds = [
+        "events/NodeLeave.xsd",
+        "events/NodeCapabilities.xsd",
+        "events/NodeAnnounce.xsd",
+        "events/NodeStatus.xsd",
+        "events/Inventory.xsd"
+        ]
+
+# Combine style sheet to use
+stylesheet = "events.xsl"
+
+##
+## This code is copied from the original gosa-ng source, with a few modification to work
+## with the given test scenario.
+##
+eventsxml = "<events>"
+for file_path in xsds:
+    eventsxml += '<path name="%s">%s</path>' % (os.path.splitext(os.path.basename(file_path))[0], file_path)
+eventsxml += '</events>'
+
+# Parse the string with all event paths
+eventsxml = StringIO.StringIO(eventsxml)
+xml_doc = etree.parse(eventsxml)
+
+# Parse XSLT stylesheet and create a transform object
+xslt_doc = etree.parse(stylesheet)
+transform = etree.XSLT(xslt_doc)
+
+# Transform the tree of all event paths into the final XSD
+res = transform(xml_doc)
+
+# ----
+
+# Initialize parser
+schema_root = etree.XML(str(res))
+schema = etree.XMLSchema(schema_root)
+parser = objectify.makeparser(schema=schema)
+
+
+##
+## Try to validate the test-event files
+##
+try:
+    d = objectify.fromstring(open('tests/valid.xml').read(), parser)
+    print "OK: Schema file tests/valid.xml is valid! (As expected)"
+except Exception as e:
+    print "FAILED: Schema file tests/valid.xml is INvalid! (Unexpected!!)"
+    print e
+
+try:
+    d = objectify.fromstring(open('tests/invalid.xml').read(), parser)
+    print "FAILED: Schema file tests/valid.xml is valid! (Unexpected!!)"
+except Exception as e:
+    print "OK: Schema file tests/valid.xml is INvalid! (As expected)"
+    print e
+
