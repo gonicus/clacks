aa1e278569ec5d44f51cfa5c9550dbab215d1d5e
Fabian Hickert <fabian.hickert@gonicus.de>
2011-11-11 12:00:24 +0100

    Updated dbxml

--- contrib/xmldb/test.py
+++ contrib/xmldb/test.py
@@ -19,22 +19,37 @@ if __name__ == "__main__":
     else:
         print "XML DB erstellt!"
         cont = mgr.createContainer(containerName)
-        cont.putDocument(r"Inventory", open('xml_content.xml').read(), uc)
+        cont.putDocument(r"garnele-2011-10-27-16-23-21", open('xml_content.xml').read(), uc)
+        cont.putDocument(r"independence-2011-10-27-16-19-50", open('xml_content2.xml').read(), uc)
 
+    # Create query context and populate used namespaces
     qc = mgr.createQueryContext()
     qc.setNamespace("", "http://www.gonicus.de/Events")
     qc.setNamespace("gosa", "http://www.gonicus.de/Events")
     qc.setNamespace("xsi","http://www.w3.org/2001/XMLSchema-instance")
-    results = mgr.query("collection('inventory.dbxml')/Event/Inventory/DeviceID", qc)
-    results.reset()
-    for value in results:
-        document = value.asDocument()
-        print document.getName(), "=", value.asString()
 
-    results = mgr.query("collection('inventory.dbxml')//Memory", qc)
+    # Query for the used DeviceIDs
+    print
+    print "Searching for client-inventory data ..."
+    results = mgr.query("collection('inventory.dbxml')/Event/Inventory/DeviceID/string()", qc)
     results.reset()
+    print "Found client inventory data sets for:"
     for value in results:
-        document = value.asDocument()
-        print document.getName(), "=", value.asString()
+        print " * %s" % (value.asString(),)
+
+        # Query for garneles
+        results2 = mgr.query("collection('inventory.dbxml')/Event/Inventory[DeviceID='%s']/ClientVersion/string()" % (value.asString(),), qc)
+        results2.reset()
+        for value in results2:
+            print "   is using client version: %s" % (value.asString(),)
+
+        print
+
+    # # Query for all used 'Memory' tags
+    # results = mgr.query("collection('inventory.dbxml')//Memory", qc)
+    # results.reset()
+    # for value in results:
+    #     document = value.asDocument()
+    #     print document.getName(), "=", value.asString()
 
 
