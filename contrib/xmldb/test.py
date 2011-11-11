0c54da4af8d0e7e5aa28697e84b4ae3a7878a96d
Fabian Hickert <fabian.hickert@gonicus.de>
2011-11-11 09:04:20 +0100

    Added xmldb test

--- contrib/xmldb/test.py
+++ contrib/xmldb/test.py
@@ -7,20 +7,34 @@ from dbxml import *
 import sys
 if __name__ == "__main__":
 
-    containerName = "inventory.dbxml"
+    containerName = r"inventory.dbxml"
 
     mgr = XmlManager()
+    mgr.removeContainer(containerName)
 
-    #mgr.removeContainer(containerName)
+    uc = mgr.createUpdateContext()
     if mgr.existsContainer(containerName) != 0:
         print "XML DB existiert bereits!"
         cont = mgr.openContainer(containerName)
     else:
         print "XML DB erstellt!"
         cont = mgr.createContainer(containerName)
+        cont.putDocument(r"Inventory", open('xml_content.xml').read(), uc)
 
-    print cont
+    qc = mgr.createQueryContext()
+    qc.setNamespace("", "http://www.gonicus.de/Events")
+    qc.setNamespace("gosa", "http://www.gonicus.de/Events")
+    qc.setNamespace("xsi","http://www.w3.org/2001/XMLSchema-instance")
+    results = mgr.query("collection('inventory.dbxml')/Event/Inventory/DeviceID", qc)
+    results.reset()
+    for value in results:
+        document = value.asDocument()
+        print document.getName(), "=", value.asString()
 
-
+    results = mgr.query("collection('inventory.dbxml')//Memory", qc)
+    results.reset()
+    for value in results:
+        document = value.asDocument()
+        print document.getName(), "=", value.asString()
 
 
