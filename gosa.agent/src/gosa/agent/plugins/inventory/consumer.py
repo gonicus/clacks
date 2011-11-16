0092bd92168992745403b8cab0ec6575fa5e4ffb
Fabian Hickert <fabian.hickert@gonicus.de>
2011-11-16 09:39:53 +0100

    Updated invent consumer plugin

--- gosa.agent/src/gosa/agent/plugins/inventory/consumer.py
+++ gosa.agent/src/gosa/agent/plugins/inventory/consumer.py
@@ -8,7 +8,11 @@ from dbxml import *
 from bsddb3.db import *
 from lxml import etree, objectify
 from gosa.common import Environment
-from gosa.common.components import AMQPEventConsumer
+from gosa.common.components import AMQPEventConsumer, Plugin
+from gosa.common.components.amqp import EventConsumer
+from gosa.common.components.registry import PluginRegistry
+from gosa.agent.plugins.inventory.dbxml import InventoryDBXml
+
 
 
 class InventoryException(Exception):
@@ -18,88 +22,12 @@ class InventoryException(Exception):
     pass
 
 
-class InventoryDBXml(object):
-    """
-    GOsa client-inventory database based on DBXML
-    """
-
-    dbpath = None
-    manager = None
-    updateContext = None
-    queryContext = None
-    container = None
-
-    def __init__(self, dbpath):
-        """
-        Creates and establishes a dbxml container connection.
-        """
-
-        # Ensure that the given dbpath is accessible
-        if not os.path.exists(os.path.dirname(dbpath)):
-            os.makedirs(os.path.dirname(dbpath))
-
-        # External access is required to validate against a given xml-schema
-        self.manager = XmlManager(DBXML_ALLOW_EXTERNAL_ACCESS)
-
-        # Open (create) the database container
-        self.container = self.manager.openContainer(dbpath, DB_CREATE|DBXML_ALLOW_VALIDATION)
-        self.dbpath = "dbxml:///%s" % (dbpath,)
-
-        # Create the update context, it is required to query and manipulate data later.
-        self.updateContext = self.manager.createUpdateContext()
-
-        # Create query context and set namespaces
-        self.queryContext = self.manager.createQueryContext()
-        self.queryContext.setNamespace("", "http://www.gonicus.de/Events")
-        self.queryContext.setNamespace("gosa", "http://www.gonicus.de/Events")
-        self.queryContext.setNamespace("xsi", "http://www.w3.org/2001/XMLSchema-instance")
-
-    def uuidExists(self, uuid):
-        """
-        Checks whether an inventory exists for the given client ID or not.
-        """
-        results = self.manager.query("collection('%s')/Event/Inventory[ClientUUID='%s']/ClientUUID/string()" % (
-            self.dbpath, uuid), self.queryContext)
-
-        # Walk through results if there are any and return True in that case.
-        results.reset()
-        for value in results:
-            return True
-        return False
-
-    def getChecksumByUuid(self, uuid):
-        """
-        Returns the checksum of a specific entry.
-        """
-        results = self.manager.query("collection('%s')/Event/Inventory[ClientUUID='%s']/GOsaChecksum/string()" % (
-            self.dbpath, uuid), self.queryContext)
-
-        # Walk through results and return the found checksum
-        results.reset()
-        for value in results:
-            return value.asString()
-        return None
-
-    def addClientInventoryData(self, uuid, data):
-        """
-        Adds client inventory data to the database.
-        """
-        self.container.putDocument(uuid, data, self.updateContext)
-
-    def deleteByUUID(self, uuid):
-        results = self.manager.query("collection('%s')/Event/Inventory[ClientUUID='%s']" % (self.dbpath, uuid), self.queryContext)
-        results.reset()
-        for value in results:
-            self.container.deleteDocument(value.asDocument().getName(), self.updateContext)
-        return None
-
-# --------------------------
-
-
-class InventoryConsumer(object):
+class InventoryConsumer(Plugin):
     """
     Consumer for inventory events emitted from clients.
     """
+
+    _target_ = 'inventory'
 
     xmldb = None
     log = None
@@ -116,6 +44,17 @@ class InventoryConsumer(object):
 
         # Let the user know that things went fine
         self.log.info("Client-inventory databases successfully initialized")
+
+        # Create event consumer
+        amqp = PluginRegistry.getInstance('AMQPHandler')
+        EventConsumer(self.env,
+            amqp.getConnection(),
+            xquery="""
+                declare namespace f='http://www.gonicus.de/Events';
+                let $e := ./f:Event
+                return $e/f:Inventory
+            """,
+            callback=self.process)
 
     def process(self, data):
         """
---
+++ gosa.agent/src/gosa/agent/plugins/inventory/dbxml.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import logging
+from dbxml import *
+from bsddb3.db import *
+from lxml import etree, objectify
+from gosa.common import Environment
+
+
+class InventoryDBXml(object):
+    """
+    GOsa client-inventory database based on DBXML
+    """
+
+    dbpath = None
+    manager = None
+    updateContext = None
+    queryContext = None
+    container = None
+
+    def __init__(self, dbpath):
+        """
+        Creates and establishes a dbxml container connection.
+        """
+
+        # Ensure that the given dbpath is accessible
+        if not os.path.exists(os.path.dirname(dbpath)):
+            os.makedirs(os.path.dirname(dbpath))
+
+        # External access is required to validate against a given xml-schema
+        self.manager = XmlManager(DBXML_ALLOW_EXTERNAL_ACCESS)
+
+        # Open (create) the database container
+        self.container = self.manager.openContainer(dbpath, DB_CREATE|DBXML_ALLOW_VALIDATION)
+        self.dbpath = "dbxml:///%s" % (dbpath,)
+
+        # Create the update context, it is required to query and manipulate data later.
+        self.updateContext = self.manager.createUpdateContext()
+
+        # Create query context and set namespaces
+        self.queryContext = self.manager.createQueryContext()
+        self.queryContext.setNamespace("", "http://www.gonicus.de/Events")
+        self.queryContext.setNamespace("gosa", "http://www.gonicus.de/Events")
+        self.queryContext.setNamespace("xsi", "http://www.w3.org/2001/XMLSchema-instance")
+
+    def uuidExists(self, uuid):
+        """
+        Checks whether an inventory exists for the given client ID or not.
+        """
+        results = self.manager.query("collection('%s')/Event/Inventory[ClientUUID='%s']/ClientUUID/string()" % (
+            self.dbpath, uuid), self.queryContext)
+
+        # Walk through results if there are any and return True in that case.
+        results.reset()
+        for value in results:
+            return True
+        return False
+
+    def getChecksumByUuid(self, uuid):
+        """
+        Returns the checksum of a specific entry.
+        """
+        results = self.manager.query("collection('%s')/Event/Inventory[ClientUUID='%s']/GOsaChecksum/string()" % (
+            self.dbpath, uuid), self.queryContext)
+
+        # Walk through results and return the found checksum
+        results.reset()
+        for value in results:
+            return value.asString()
+        return None
+
+    def addClientInventoryData(self, uuid, data):
+        """
+        Adds client inventory data to the database.
+        """
+        self.container.putDocument(uuid, data, self.updateContext)
+
+    def deleteByUUID(self, uuid):
+        results = self.manager.query("collection('%s')/Event/Inventory[ClientUUID='%s']" % (self.dbpath, uuid), self.queryContext)
+        results.reset()
+        for value in results:
+            self.container.deleteDocument(value.asDocument().getName(), self.updateContext)
+        return None
