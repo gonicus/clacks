b43956319664f63ce91dd94f0b43620c8de5766a
Fabian Hickert <fabian.hickert@gonicus.de>
2011-11-22 16:54:57 +0100

    Updated xml test

--- contrib/factory/xml-factory.py
+++ contrib/factory/xml-factory.py
@@ -10,29 +10,72 @@ from gosa.agent.objects.index import ObjectIndex, SCOPE_SUB
 # Do some searching
 ie = ObjectIndex()
 
-print "*" * 80
-print "Receive"
-print "*" * 80
-
-obj = GOsaObjectProxy(u"cn=Claudia Mustermann,ou=people,dc=gonicus,dc=de")
-
-print "  Parent DN:", obj.get_parent_dn()
-print "  Type:", obj.get_base_type()
-print "  Extensions:", ", ".join(e for e, i in obj.get_extension_types().items() if i)
-print "  Given name:", obj.givenName
-print "  Last name:", obj.sn
-
-print "*" * 80
-print "Receive object as xml"
-print "*" * 80
+import pprint
 
 
-xml = obj.asXml()
+l = [
+    'cn=Rainer Luelsdorf,ou=people,ou=GL,dc=gonicus,dc=de',
+    'cn=Lars Scheiter,ou=people,ou=Technik,dc=gonicus,dc=de',
+    'cn=Susanne Korb,ou=people,ou=Vertrieb,dc=gonicus,dc=de',
+    'cn=Cajus Pollmeier,ou=people,ou=Technik,dc=gonicus,dc=de',
+    'cn=GONICUS GONICUS,ou=people,dc=gonicus,dc=de',
+    'cn=Admin GOforge,ou=people,ou=Administrativa,dc=gonicus,dc=de',
+    'cn=Nagios Monitor,ou=people,ou=Technik,dc=gonicus,dc=de',
+    'cn=Vertriebs FAX,ou=people,ou=Vertrieb,dc=gonicus,dc=de',
+    'cn=Markus Frielinghausen,ou=people,ou=Technik,dc=gonicus,dc=de',
+    'cn=Andre Grabowsky,ou=people,ou=Extern,dc=gonicus,dc=de',
+    'cn=Fabian Hickert,ou=people,ou=Technik,dc=gonicus,dc=de',
+    'cn=Fabian Hickert,ou=people,ou=Technik,dc=gonicus,dc=de',
+    'cn=Christian Patsch,ou=people,ou=Consulting,dc=gonicus,dc=de',
+    'cn=Christian Patsch,ou=people,ou=Consulting,dc=gonicus,dc=de',
+    'cn=Christian Patsch,ou=people,ou=Consulting,dc=gonicus,dc=de',
+    'cn=Carsten Sommer,ou=people,ou=Technik,dc=gonicus,dc=de',
+    'cn=Simon Fischer,ou=people,ou=Extern,dc=gonicus,dc=de',
+    'cn=Projekt Management,ou=people,ou=Vertrieb,dc=gonicus,dc=de',
+    'cn=Hans-Peter Zerres,ou=people,ou=Technik,dc=gonicus,dc=de',
+    'cn=Stefan Grote,ou=people,ou=Vertrieb,dc=gonicus,dc=de',
+    'cn=Karl Postmann,ou=people,dc=gonicus,dc=de',
+    'cn=Jan Wischnat,ou=people,ou=Extern,dc=gonicus,dc=de',
+    'cn=Sebastian Denz,ou=people,ou=Technik,dc=gonicus,dc=de',
+    'cn=Tester Tester,ou=people,dc=gonicus,dc=de',
+    'cn=goforgebugs goforgebugs,ou=people,dc=gonicus,dc=de',
+    'cn=gometers gometers,ou=people,dc=gonicus,dc=de',
+    'cn=Axel Jahn,ou=people,ou=Extern,dc=gonicus,dc=de',
+    'cn=Steven Rines,ou=people,ou=Extern,dc=gonicus,dc=de',
+    'cn=presenter presenter,ou=people,ou=Extern,dc=gonicus,dc=de',
+    'cn=Jan Wenzel,ou=people,ou=Technik,dc=gonicus,dc=de',
+    'cn=Gerd Mayr,ou=people,ou=Extern,dc=gonicus,dc=de',
+    'cn=Raum Dreiblitz,ou=people,dc=gonicus,dc=de',
+    'cn=Hans Wurst,ou=people,ou=Testbed,dc=gonicus,dc=de',
+    'cn=Go Bynari,ou=people,dc=gonicus,dc=de',
+    'cn=go bynari2,ou=people,dc=gonicus,dc=de',
+    'cn=bynari3 go,ou=people,dc=gonicus,dc=de',
+    'cn=Michael Leppke,ou=people,ou=Auszubildende,dc=gonicus,dc=de',
+    'cn=Stefan Japes,ou=people,ou=Technik,dc=gonicus,dc=de',
+    'cn=Server Raum,ou=people,ou=Phones,ou=Technik,dc=gonicus,dc=de',
+    'uid=ADMIN-1$,ou=winstations,ou=systems,dc=gonicus,dc=de',
+    'uid=FS-1$,ou=winstations,ou=systems,dc=gonicus,dc=de',
+    'uid=WS-WINXP02$,ou=winstations,ou=systems,dc=gonicus,dc=de',
+    'uid=XP2TEST$,ou=winstations,ou=systems,dc=gonicus,dc=de',
+    'cn=Webadmin Desch,ou=people,ou=Webspace,ou=Extern,dc=gonicus,dc=de',
+    'cn=GONICUS Webadmin,ou=people,ou=Webspace,ou=Extern,dc=gonicus,dc=de',
+    'cn=Konrad Kleine,ou=people,ou=Technik,dc=gonicus,dc=de',
+    'uid=INDEPENDENCE$,ou=winstations,ou=systems,dc=gonicus,dc=de',
+    'cn=GO Alfresco,ou=people,dc=gonicus,dc=de']
 
-schema_root = etree.XML(etree.tostring(obj.getXmlObjectSchema(), pretty_print=True))
-#schema_root = etree.XML(open('a').read())
-schema = etree.XMLSchema(schema_root)
-parser = objectify.makeparser(schema=schema)
-xml = objectify.fromstring(xml, parser)
 
-print etree.tostring(xml, pretty_print=True)
+# Create parse for objects
+#schema_root = etree.XML(etree.tostring(obj.getXmlObjectSchema(), pretty_print=True))
+#schema = etree.XMLSchema(schema_root)
+#parser = objectify.makeparser(schema=schema)
+
+for entry in l:
+    print entry
+    try:
+        obj = GOsaObjectProxy(entry)
+        xml = obj.asXml()
+    except Exception as e:
+        print e
+
+    #xml = objectify.fromstring(xml, parser)
+
