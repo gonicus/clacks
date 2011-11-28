2a69f1219d2113e324c44ccfc7b3f69f76f6b252
Fabian Hickert <hickert@gonicus.de>
2011-11-28 03:21:51 -0500

    Updated dbxml tests

--- contrib/xmldbs/test.py
+++ contrib/xmldbs/test.py
@@ -8,14 +8,20 @@ def test_db(db):
     print type(db)
     print "=" *60
 
+    print "Set namespaces"
     db.setNamespace("gosa", "http://www.gonicus.de/Objects")
     db.setNamespace("xsi", "http://www.w3.org/2001/XMLSchema-instance")
 
+    print "Drop database if exists"
     if db.databaseExists('host'):
+        print "... exists"
         db.dropDatabase('host')
 
+    print "Create database"
     db.createDatabase('host')
+    print "Open database"
     db.openDatabase('host')
+    print "Add documents"
     db.addDocument('/world1', open("dummy.xml").read())
     db.addDocument('/world2', open("dummy2.xml").read())
     db.addDocument('/a/world2', "<a>/a/world2</a>")
@@ -25,7 +31,7 @@ def test_db(db):
     print "Exists:\t\t", db.documentExists("/a/b/c/world2")
     print "Exists:\t\t", db.documentExists("/a/b/c/world")
     print "Query: "
-    for entry in db.xquery("collection('host')/gosa:GenericUser/gosa:UUID/text()"):
+    for entry in db.xquery("collection('host')/gosa:GenericUser/gosa:Attributes/gosa:uid/text()"):
         print "\t\t", entry
     print "Query: "
     for entry in db.xquery("collection('host')/gosa:GenericUser[gosa:UUID='03a82842-ed95-102f-8038-812492b1b75c']/gosa:Extensions/gosa:Extension/text()"):
@@ -48,9 +54,9 @@ def test_db(db):
 
     print "DB Exists:\t", db.databaseExists("host")
     print "Docs: \t\t", db.getDocuments()
-    db.deleteDocument("/a/b/c/world2")
     db.deleteDocument("/a/world2")
     db.deleteDocument("/world2")
+    db.deleteDocument("/a/b/c/world2")
     db.deleteDocument("/b/world2")
     db.dropDatabase('host')
 
