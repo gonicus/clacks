2aa378327bae46f7cd4d0ea11ce24f4c332ce138
Fabian Hickert <hickert@gonicus.de>
2011-12-16 07:41:25 -0500

    Updated dbus service forwarder

--- client/src/gosa/client/plugins/service/utils.py
+++ client/src/gosa/client/plugins/service/utils.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import dbus
+import re
 from gosa.common import Environment
 from gosa.common.components import Plugin
 from gosa.common.components import Command
@@ -12,20 +13,35 @@ class Service(Plugin):
     and all its services.
     """
     _target_ = 'service'
+    env = None
+    bus = None
+    gosa_dbus = None
+    methods = None
 
     def __init__(self):
         env = Environment.getInstance()
         self.env = env
+        self.bus = dbus.SystemBus()
+        self.gosa_dbus = self.bus.get_object('com.gonicus.gosa', '/com/gonicus/gosa/service')
+        call = self.gosa_dbus._Introspect()
+        call.block()
 
-    @Command()
-    def setRunlevel(self, runlevel):
+        # Collection methods
+        self.methods = {}
+        for method in self.gosa_dbus._introspect_method_map:
+            if not re.match("^com\.gonicus\.gosa\.", method):
+                continue
+            name = re.sub("^com\.gonicus\.gosa\.(.*)$", "\\1", method)
+            self.methods[name] = self.gosa_dbus._introspect_method_map[method]
+
+    def call_dbus_method(self, method, *args):
         """
         This method forwards the call the clacks-dbus
         """
-        #TODO comment
-        bus = dbus.SystemBus()
-        gosa_dbus = bus.get_object('com.gonicus.gosa', '/com/gonicus/gosa/service')
-        result = gosa_dbus.set_runlevel(runlevel, dbus_interface="com.gonicus.gosa")
-        print "Result:", result
-        return result
 
+        # Check if requested dbus method is registered.
+        if method not in self.methods:
+            raise NotImplementedError(method)
+
+        print "Calling", method, args
+        return True
