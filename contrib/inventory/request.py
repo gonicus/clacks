f491a158c21da888256a78961f0bea56860e8a21
Fabian Hickert <hickert@gonicus.de>
2011-12-20 08:46:12 -0500

    Updated DBus service

--- contrib/inventory/request.py
+++ contrib/inventory/request.py
@@ -1,23 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from pprint import pprint
 from json import loads
 from gosa.common.components import AMQPServiceProxy
 proxy = AMQPServiceProxy('amqps://cajus:tester@amqp.intranet.gonicus.de/org.gosa')
-proxy.clientDispatch("49cb1287-db4b-4ddf-bc28-5f4743eac594", "callDBusMethod", "set_runlevel", 2)
 
 
-# Stop apache service
-print "Stop"
-print proxy.clientDispatch("49cb1287-db4b-4ddf-bc28-5f4743eac594", "callDBusMethod", "stop", "apache2")
-print " .. done"
-print "Status: "
-print loads(str(proxy.clientDispatch("49cb1287-db4b-4ddf-bc28-5f4743eac594", "callDBusMethod", "get_services")))['apache2']['running']
-print "Start"
-print proxy.clientDispatch("49cb1287-db4b-4ddf-bc28-5f4743eac594", "callDBusMethod", "start", "apache2")
-print " .. done"
-print "Status: "
-print loads(str(proxy.clientDispatch("49cb1287-db4b-4ddf-bc28-5f4743eac594", "callDBusMethod", "get_services")))['apache2']['running']
+#proxy.clientDispatch("49cb1287-db4b-4ddf-bc28-5f4743eac594", "callDBusMethod", "set_runlevel", 2)
 
-print "---"
+pprint( proxy.clientDispatch("49cb1287-db4b-4ddf-bc28-5f4743eac594", "callDBusMethod", "get_service", "apache2"))
+pprint(proxy.clientDispatch("49cb1287-db4b-4ddf-bc28-5f4743eac594", "callDBusMethod", "get_services"))
+pprint(proxy.clientDispatch("49cb1287-db4b-4ddf-bc28-5f4743eac594", "callDBusMethod", "stop", "apache2"))
+pprint(proxy.clientDispatch("49cb1287-db4b-4ddf-bc28-5f4743eac594", "callDBusMethod", "start", "apache2"))
+pprint(proxy.clientDispatch("49cb1287-db4b-4ddf-bc28-5f4743eac594", "callDBusMethod", "restart", "apache2"))
+pprint(proxy.clientDispatch("49cb1287-db4b-4ddf-bc28-5f4743eac594", "callDBusMethod", "reload", "apache2"))
 
-print loads(str(proxy.clientDispatch("49cb1287-db4b-4ddf-bc28-5f4743eac594", "callDBusMethod", "get_service", "apache2")))
+
