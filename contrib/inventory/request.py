7a7371bfa355cbfc074cd6ef1014060e384fbf9c
Fabian Hickert <hickert@gonicus.de>
2011-12-20 03:50:26 -0500

    Updated DBUS Service plugin

--- contrib/inventory/request.py
+++ contrib/inventory/request.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from json import loads
 from gosa.common.components import AMQPServiceProxy
 proxy = AMQPServiceProxy('amqps://cajus:tester@amqp.intranet.gonicus.de/org.gosa')
 proxy.clientDispatch("49cb1287-db4b-4ddf-bc28-5f4743eac594", "callDBusMethod", "set_runlevel", 2)
-print proxy.clientDispatch("49cb1287-db4b-4ddf-bc28-5f4743eac594", "callDBusMethod", "get_runlevel")
+
+
+# Stop apache service
+print "Stop"
+print proxy.clientDispatch("49cb1287-db4b-4ddf-bc28-5f4743eac594", "callDBusMethod", "stop", "apache2")
+print " .. done"
+print "Status: "
+print loads(str(proxy.clientDispatch("49cb1287-db4b-4ddf-bc28-5f4743eac594", "callDBusMethod", "get_services")))['apache2']['running']
+print "Start"
+print proxy.clientDispatch("49cb1287-db4b-4ddf-bc28-5f4743eac594", "callDBusMethod", "start", "apache2")
+print " .. done"
+print "Status: "
+print loads(str(proxy.clientDispatch("49cb1287-db4b-4ddf-bc28-5f4743eac594", "callDBusMethod", "get_services")))['apache2']['running']
--- dbus/src/gosa/dbus/plugins/services/main.py
+++ dbus/src/gosa/dbus/plugins/services/main.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 import dbus.service
+from os.path import basename
 from gosa.common import Environment
 from gosa.common.components import Plugin
 from gosa.dbus import get_system_bus
+from json import dumps, loads
 import re
 import sys
 import traceback
@@ -26,12 +28,14 @@ class DBusUnixServiceHandler(dbus.service.Object, Plugin):
         self.svc_command = self.env.config.get("unix.service-command", default="/usr/sbin/service")
 
     def _validate(self, name, action=None):
-        services = self.get_services()
+        services = loads(str(self.get_services()))
         if not name in services:
             raise ServiceException("unknown service %s" % name)
 
         if action and not action in services[name]['actions']:
             raise ServiceException("action '%s' not supported for service %s" % (action, name))
+
+        return services[name]
 
     @dbus.service.method('com.gonicus.gosa', out_signature='i')
     def get_runlevel(self):
@@ -41,7 +45,7 @@ class DBusUnixServiceHandler(dbus.service.Object, Plugin):
 
         # Call 'who -r' and parse the return value to get the run-level
         # run-level 2  Dec 19 01:21                   last=S
-        process = subprocess.Popen(["/usr/bin/who","-r"], env={'LC_ALL': 'C'}, \
+        process = subprocess.Popen(["who","-r"], env={'LC_ALL': 'C'}, \
                 shell=False, stdout=subprocess.PIPE,  stderr=subprocess.PIPE)
         ret = process.communicate()
         rl = re.sub("^run-level[ ]*([0-9]*).*$","\\1", ret[0].strip())
@@ -49,10 +53,12 @@ class DBusUnixServiceHandler(dbus.service.Object, Plugin):
 
     @dbus.service.method('com.gonicus.gosa', in_signature='i', out_signature='i')
     def set_runlevel(self, level):
-        #TODO
-        # init $level
-
-        return 0
+        """
+        Sets a new runlevel for the clacks-client
+        """
+        process = subprocess.Popen(["telinit","%s" % (str(level))], shell=False, stdout=subprocess.PIPE,  stderr=subprocess.PIPE)
+        ret = process.communicate()
+        return process.returncode
 
     @dbus.service.method('com.gonicus.gosa', in_signature='s', out_signature='b')
     def start(self, name):
@@ -102,6 +108,38 @@ class DBusUnixServiceHandler(dbus.service.Object, Plugin):
     @dbus.service.method('com.gonicus.gosa', out_signature='s')
     def get_services(self):
 
+        level = self.get_runlevel()
+        process = subprocess.Popen(["run-parts", "--test", "--regex=^S*", "/etc/rc%s.d" % (str(level))],
+                shell=False, stdout=subprocess.PIPE,  stderr=subprocess.PIPE, env={'LC_ALL': 'C'})
+        ret = process.communicate()
+
+        services = {}
+        for entry in ret[0].split("\n"):
+            sname = re.sub("^S[0-9]*", "", basename(entry))
+            if not sname:
+                continue
+
+            _svcs = subprocess.Popen([self.svc_command, sname], env={'LC_ALL': 'C'},
+                    stderr=subprocess.PIPE, stdout=subprocess.PIPE).communicate()
+            content = _svcs[0] + _svcs[1]
+
+            res = re.findall("(([a-zA-Z\-]*)\|)", content, re.MULTILINE) + re.findall("(\|([a-zA-Z\-]*))", content, re.MULTILINE)
+            l = set()
+            for entry in res:
+                l |= set([entry[1]],)
+
+            services[sname] = {'actions': list(l)}
+            if 'status' in services[sname]['actions']:
+                _svcs = subprocess.Popen([self.svc_command, sname, 'status'], env={'LC_ALL': 'C'},
+                        stderr=subprocess.PIPE, stdout=subprocess.PIPE).communicate()
+
+                services[sname]['running'] = re.search('is running', _svcs[0]+_svcs[1], flags=re.IGNORECASE) != None
+
+        return dumps(services)
+
+
+        #service = self._validate(name, "restart")
+        #return subprocess.call([self.svc_command, name, 'restart']) == 0
         #TODO: change this from the current implementation to:
         #      get_runlevel()
         #      for service in /etc/rc%level%.d/S* (not really, see "man run-parts" for more information)
@@ -109,24 +147,24 @@ class DBusUnixServiceHandler(dbus.service.Object, Plugin):
         #          if usage supports "status", run "$svc_command $service status" to find out if it's running
         #          if there is an icon for $service.(png|gif|jpeg), save the path
 
-        services = {}
-        state = re.compile(r"^\s*\[\s+([?+-])\s+\]\s+([^\s]+)\s*$")
+        #services = {}
+        #state = re.compile(r"^\s*\[\s+([?+-])\s+\]\s+([^\s]+)\s*$")
 
-        # Das weg, dann siehe Todo
-        _svcs = subprocess.Popen([self.svc_command, '--status-all'],
-                stderr=subprocess.PIPE,
-                stdout=subprocess.PIPE).communicate()
-        svcs = (_svcs[0] + _svcs[1]).split('\n')
+        ## Das weg, dann siehe Todo
+        #_svcs = subprocess.Popen([self.svc_command, '--status-all'],
+        #        stderr=subprocess.PIPE,
+        #        stdout=subprocess.PIPE).communicate()
+        #svcs = (_svcs[0] + _svcs[1]).split('\n')
 
-        for svc in svcs:
-            try:
-                status, service = state.match(svc).groups()
-                services[service] = {
-                        'running': self.__status_map[status],
-                        'methods': ['start', 'stop', 'restart', 'reload'],
-                        'icon': None}
+        #for svc in svcs:
+        #    try:
+        #        status, service = state.match(svc).groups()
+        #        services[service] = {
+        #                'running': self.__status_map[status],
+        #                'methods': ['start', 'stop', 'restart', 'reload'],
+        #                'icon': None}
 
-            except AttributeError:
-                pass
+        #    except AttributeError:
+        #        pass
 
-        return services
+        #return services
