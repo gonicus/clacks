1850ffcd2d3093fe31dc4658f340b417885bde2b
Fabian Hickert <hickert@gonicus.de>
2011-12-20 04:38:05 -0500

    Updated dbus service plugin

--- dbus/src/gosa/dbus/plugins/services/main.py
+++ dbus/src/gosa/dbus/plugins/services/main.py
@@ -97,74 +97,56 @@ class DBusUnixServiceHandler(dbus.service.Object, Plugin):
 
         return subprocess.call([self.svc_command, name, 'reload']) == 0
 
-    @dbus.service.method('com.gonicus.gosa', in_signature='s', out_signature='a{ss}')
+    #@dbus.service.method('com.gonicus.gosa', in_signature='s', out_signature='a{ss}')
+    @dbus.service.method('com.gonicus.gosa', in_signature='s', out_signature='s')
     def get_service(self, name):
-        services = self.get_services()
+        services = loads(str(self.get_services()))
         if not name in services:
             raise NoSuchServiceException("unknown service %s" % name)
 
-        return services[name]
+        return dumps(services[name])
 
     @dbus.service.method('com.gonicus.gosa', out_signature='s')
     def get_services(self):
 
+        # Get the current runlevel and then check for registered services using
+        #  run-parts --test --regex=^S* /etc/rc<level>.d
         level = self.get_runlevel()
         process = subprocess.Popen(["run-parts", "--test", "--regex=^S*", "/etc/rc%s.d" % (str(level))],
                 shell=False, stdout=subprocess.PIPE,  stderr=subprocess.PIPE, env={'LC_ALL': 'C'})
         ret = process.communicate()
 
+        # Parse results and strip out path infos and S[0-9] prefix
         services = {}
         for entry in ret[0].split("\n"):
             sname = re.sub("^S[0-9]*", "", basename(entry))
+
+            # Do not add empty service names
             if not sname:
                 continue
 
+            # Try to detect the service actions we can perform (e.g. start/stop)
             _svcs = subprocess.Popen([self.svc_command, sname], env={'LC_ALL': 'C'},
                     stderr=subprocess.PIPE, stdout=subprocess.PIPE).communicate()
             content = _svcs[0] + _svcs[1]
 
+            # Search useable service actions in the result
             res = re.findall("(([a-zA-Z\-]*)\|)", content, re.MULTILINE) + re.findall("(\|([a-zA-Z\-]*))", content, re.MULTILINE)
             l = set()
             for entry in res:
                 l |= set([entry[1]],)
 
-            services[sname] = {'actions': list(l)}
+            # Create a service entry for the result
+            services[sname] = {'actions': list(l), 'running': None, 'icon': None}
+
+            # Check if the service running by calling 'status', if available
             if 'status' in services[sname]['actions']:
                 _svcs = subprocess.Popen([self.svc_command, sname, 'status'], env={'LC_ALL': 'C'},
                         stderr=subprocess.PIPE, stdout=subprocess.PIPE).communicate()
 
+                # Search for a string which tells us that the service is running
+                # Be careful some infos return values like this:
+                #  * isn't running | is not running | not running | failed (running) ...
                 services[sname]['running'] = re.search('is running', _svcs[0]+_svcs[1], flags=re.IGNORECASE) != None
 
         return dumps(services)
-
-
-        #service = self._validate(name, "restart")
-        #return subprocess.call([self.svc_command, name, 'restart']) == 0
-        #TODO: change this from the current implementation to:
-        #      get_runlevel()
-        #      for service in /etc/rc%level%.d/S* (not really, see "man run-parts" for more information)
-        #          run "$svc_command $service" to find out about the usage
-        #          if usage supports "status", run "$svc_command $service status" to find out if it's running
-        #          if there is an icon for $service.(png|gif|jpeg), save the path
-
-        #services = {}
-        #state = re.compile(r"^\s*\[\s+([?+-])\s+\]\s+([^\s]+)\s*$")
-
-        ## Das weg, dann siehe Todo
-        #_svcs = subprocess.Popen([self.svc_command, '--status-all'],
-        #        stderr=subprocess.PIPE,
-        #        stdout=subprocess.PIPE).communicate()
-        #svcs = (_svcs[0] + _svcs[1]).split('\n')
-
-        #for svc in svcs:
-        #    try:
-        #        status, service = state.match(svc).groups()
-        #        services[service] = {
-        #                'running': self.__status_map[status],
-        #                'methods': ['start', 'stop', 'restart', 'reload'],
-        #                'icon': None}
-
-        #    except AttributeError:
-        #        pass
-
-        #return services
