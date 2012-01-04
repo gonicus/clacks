db1a7c8fcc164dafadfe7438814f9da3b9cce3f3
Cajus Pollmeier <pollmeier@gonicus.de>
2012-01-04 11:21:09 +0100

    moved fts to separate project

--- contrib/pxe/fts/src/fts/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-__import__('pkg_resources').declare_namespace(__name__)
-from fts.config import Config
-from fts.filestat import FileStat
-from fts.pxefs import PXEfs
-from fts.plugins.interface import BootPlugin
