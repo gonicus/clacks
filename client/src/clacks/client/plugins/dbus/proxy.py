efed12399927db9058dddfe8f8024703e37a5ab7
Fabian Hickert <hickert@gonicus.de>
2012-01-11 05:58:51 -0500

    Added parameter checking to the dbus-proxy-plugin of the client

--- client/src/clacks/client/plugins/dbus/proxy.py
+++ client/src/clacks/client/plugins/dbus/proxy.py
@@ -35,6 +35,20 @@ class DBUSProxy(Plugin):
     clacks_dbus = None
     methods = None
 
+    # Type map for signature checking
+    _type_map = {
+                 'i': [int],
+                 'n': [int],
+                 'x': [int],
+                 'q': [int],
+                 'y': [chr],
+                 'u': [int],
+                 't': [int],
+                 'b': [bool],
+                 's': [unicode, str],
+                 'o': [object],
+                 'd': [float]}
+
     def __init__(self):
         self.log = logging.getLogger(__name__)
 
@@ -44,15 +58,16 @@ class DBUSProxy(Plugin):
         # Get a dbus proxy and check if theres a service registered called 'org.clacks'
         # if not, then we can skip all further processing. (The clacks-dbus seems not to be running)
         self.bus = dbus.SystemBus()
+        self.methods = {}
         if not service in self.bus.list_names():
-            self.log.debug("No dbus service registered for '%s'. The clacks-dbus seems not to be running!" % (service))
+            self.log.debug("no dbus service registered for '%s'. The clacks-dbus seems not to be running!" % (service))
         else:
             try:
                 self.log.debug('loading dbus-methods registered by clacks (introspection)')
                 self.methods = self._call_introspection(service, "/")
                 self.log.debug("found %s registered dbus methods" % (str(len(self.methods))))
             except DBusException as exception:
-                self.log.debug("failed to load registered dbus methods: %s" % (str(exception)))
+                self.log.debug("failed to load dbus methods (e.g. check rights in dbus config): %s" % (str(exception)))
 
     def _call_introspection(self, service, path, methods = None):
         """
@@ -99,9 +114,9 @@ class DBUSProxy(Plugin):
         # Transform received XML data into a python object.
         res = etree.fromstring(data)
 
-        # Check for a result containing dbus-method information.
+        # Check for a xml-node containing dbus-method information.
         #
-        # It will look like this:
+        # It looks like this:
         #       <node name="/org/clacks/notify">
         #         <interface name="org.clacks">
         #           <method name="notify_all">
@@ -139,8 +154,7 @@ class DBUSProxy(Plugin):
                             if arg.tag == "arg" and arg.get("direction") == "in":
                                 methods[m_name]['args'][arg.get('name')] = arg.get('type')
 
-
-        # Check for a node which introdumethod_namees new paths
+        # Check for a xml-node which introduces new paths
         #
         # It will look like this:
         #       <node>
@@ -179,14 +193,13 @@ class DBUSProxy(Plugin):
         ======= ==============
         Key     Description
         ======= ==============
-        method  The name of method to call on dbus side.
+        method  The name of the method to call on dbus side.
         ...     A list of parameters for the method call.
         ======= ==============
         """
 
-        # Check if requested dbus method is registered.
-        if method not in self.methods:
-            raise NotImplementedError(method)
+        # Check given method and parameters
+        self._check_parameters(method, args)
 
         # Now call the dbus method with the given list of paramters
         mdata = self.methods[method]
@@ -194,3 +207,54 @@ class DBUSProxy(Plugin):
         method = cdbus.get_dbus_method(method, dbus_interface=mdata['service'])
         returnval = method(*args)
         return returnval
+
+    def _check_parameters(self, method, args):
+        """
+        Checks if the given list of arguments (args) are compatible with the
+        given dbus-method (method)
+
+        ======= ==============
+        Key     Description
+        ======= ==============
+        method  The name of the method.
+        args    The list of arguments to check for.
+        ======= ==============
+        """
+
+        # Check if the requested dbus method is registered.
+        if method not in self.methods:
+            raise NotImplementedError(method)
+
+        # Get list of required argument
+        m_args = self.methods[method]['args']
+        given = ""
+        args = list(args)
+
+        # Check each argument
+        cnt = 0
+        for argument in m_args:
+            cnt += 1
+
+            # Does the argument exists
+            try:
+                given = args.pop(0)
+            except IndexError as e:
+                raise TypeError("the parameter '%s' is missing" % argument)
+
+
+            # Check if the given argument matches the required signature type
+            found = True
+            if m_args[argument] in self._type_map:
+                found = False
+                for p_type in self._type_map[m_args[argument]]:
+                    if isinstance(given, p_type):
+                        found = True
+
+            if not found:
+                types = ", ".join(map(lambda x: x.__name__, self._type_map[m_args[argument]]))
+                raise TypeError("the parameter %s (%s) is of invalid type. Expected: %s" % (argument, str(cnt), types))
+
+        # We received more arguments than required by the dbus method...
+        if len(args):
+            raise TypeError("%s() takes exactly %s arguments (%s given)" % (method, len(m_args), cnt+len(args)))
+
