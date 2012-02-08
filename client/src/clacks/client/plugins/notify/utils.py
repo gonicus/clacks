0a0bf7243286bbd33e294267304327a3ba564c70
Fabian Hickert <hickert@gonicus.de>
2012-02-08 06:48:54 -0500

    Updated calcks notify extension to register methods dynamically depnending on the dbus availability

--- client/src/clacks/client/plugins/notify/utils.py
+++ client/src/clacks/client/plugins/notify/utils.py
@@ -14,6 +14,8 @@ e.g.:
 
 # -*- coding: utf-8 -*-
 import dbus
+import logging
+from clacks.common.components import PluginRegistry
 from clacks.common.components import Plugin
 from clacks.common.components import Command
 from clacks.common import Environment
@@ -21,12 +23,51 @@ from clacks.common import Environment
 
 class Notify(Plugin):
     _target_ = 'notify'
+    bus = None
+    clacks_dbus = None
 
     def __init__(self):
         env = Environment.getInstance()
         self.env = env
+        self.log = logging.getLogger(__name__)
 
-    @Command()
+        # Register ourselfs for bus changes on org.clacks
+        self.bus = dbus.SystemBus()
+        self.bus.watch_name_owner("org.clacks", self.__dbus_proxy_monitor)
+
+    def __dbus_proxy_monitor(self, bus_name):
+        """
+        This method monitors the DBus service 'org.clacks' and whenever there is a
+        change in the status (dbus closed/startet) we will take notice.
+        And can register or unregister methods to the dbus
+        """
+        if "org.clacks" in self.bus.list_names():
+            if self.clacks_dbus:
+                del(self.clacks_dbus)
+            self.clacks_dbus = self.bus.get_object('org.clacks', '/org/clacks/notify')
+            ccr = PluginRegistry.getInstance('ClientCommandRegistry')
+            ccr.register("notify", 'Notify.notify', [], \
+                    ['user','title','message','timeout','urgency','icon','actions','recurrence'], \
+                    'Sent a notification to a given user')
+            ccr.register("notify_all", 'Notify.notify_all', [], \
+                    ['title','message','timeout','urgency','icon','actions','recurrence'], \
+                    'Sent a notification to a given user')
+            self.log.info("established dbus connection")
+
+        else:
+            if self.clacks_dbus:
+                del(self.clacks_dbus)
+                ccr = PluginRegistry.getInstance('ClientCommandRegistry')
+                ccr.unregister("notify")
+                ccr.unregister("notify_all")
+                self.log.info("lost dbus connection")
+            else:
+                self.log.info("no dbus connection")
+
+        # Trigger resend of capapability event
+        amcs = PluginRegistry.getInstance('AMQPClientService')
+        amcs.reAnnounce()
+
     def notify(self, user, title, message,
         timeout=0,
         urgency="normal",
@@ -36,17 +77,11 @@ class Notify(Plugin):
 
         """ Sent a notification to a given user """
 
-        # Get BUS connection
-        bus = dbus.SystemBus()
-        clacks_dbus = bus.get_object('org.clacks',
-                                   '/org/clacks/notify')
-
         # Send notification and keep return code
-        o = clacks_dbus._notify(user, title, message, timeout, urgency,
+        o = self.clacks_dbus._notify(user, title, message, timeout, urgency,
             icon, actions, recurrence, dbus_interface="org.clacks")
         return(int(o))
 
-    @Command()
     def notify_all(self, title, message,
         timeout=0,
         urgency="normal",
@@ -56,12 +91,7 @@ class Notify(Plugin):
 
         """ Sent a notification to all users on a machine """
 
-        # Get BUS connection
-        bus = dbus.SystemBus()
-        clacks_dbus = bus.get_object('org.clacks',
-                                   '/org/clacks/notify')
-
         # Send notification and keep return code
-        o = clacks_dbus._notify_all(title, message, timeout, urgency,
+        o = self.clacks_dbus._notify_all(title, message, timeout, urgency,
             icon, actions, recurrence, dbus_interface="org.clacks")
         return(int(o))
