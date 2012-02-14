cce9aa1faf798c453a7193e756bdf727c7abaa8d
Fabian Hickert <hickert@gonicus.de>
2012-02-14 04:26:18 -0500

    Added some more test files

--- contrib/index_search/lepl.py
+++ contrib/index_search/lepl.py
@@ -8,6 +8,7 @@ def handle_attributes(attrs):
 def handle_base(attrs):
     return attrs
 
+
 # A definition for space characters including newline and tab
 spaces = Star(Space() | '\n' | '\t')
 
@@ -18,6 +19,7 @@ attr_type = Regexp('[a-z]+') > 'type'
 attr_name = Regexp('[a-z]+') > 'name'
 
 attribute = attr_type & ~Literal('.') & attr_name > dict
+attribute_plain = attr_type & ~Literal('.') & attr_name
 
 
 # Allow a list of attributes
@@ -31,13 +33,19 @@ base_type = Regexp('[a-z]+') > 'object'
 base_value = String() > 'base'
 base = ~Literal('BASE') & ~spaces & base_type & ~spaces & base_value & ~spaces & scope_option > handle_base
 
-statement = ~spaces & select & ~spaces & base & ~spaces
+contition_statement = ( attribute_plain | String())
+operator = (Literal('=') | Literal ('!='))
+condition = attribute_plain & spaces & contition_statement & operator & ~spaces & contition_statement
+where = ~Literal('WHERE') & ~spaces & where_statement
+
+query_parser = ~spaces & select & ~spaces & base & ~spaces & where & ~spaces
 
 
-query = """SELECT asdf.asdf, herbert.tester
-BASE user "dc=gonicus,dc=de" SUB
+query = """SELECT User.sn, User.cn
+BASE User "dc=gonicus,dc=de" SUB
+WHERE User.sn = 'Hickert'
 """
 
-print statement.parse(query)
+print query_parser.parse(query)
 
 
