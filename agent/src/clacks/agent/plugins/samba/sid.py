75464047cea14121b0283132ba5d282b51f474e4
Cajus Pollmeier <pollmeier@gonicus.de>
2012-04-12 15:33:47 +0200

    Removed hard coded values

--- agent/src/clacks/agent/plugins/samba/sid.py
+++ agent/src/clacks/agent/plugins/samba/sid.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
+from clacks.common import Environment
 from clacks.agent.objects.filter import ElementFilter
+from clacks.common.components import PluginRegistry
 
 
 class GenerateSambaSid(ElementFilter):
@@ -9,14 +11,23 @@ class GenerateSambaSid(ElementFilter):
 
     def __init__(self, obj):
         super(GenerateSambaSid, self).__init__(obj)
+        self.env = Environment.getInstance()
 
     def process(self, obj, key, valDict, method, number, domain, group_type = 0):
+        index = PluginRegistry.getInstance("ObjectIndex")
 
-        #TODO: Get this information from the backend/config
-        ridbase = 1000
+        #TODO: escape domain
+        sid = index.xquery("collection('objects')/o:SambaDomain[lower-case(*/o:sambaDomainName) = lower-case('%s')]//o:sambaSID/string()" % domain)
+        if len(sid) != 1:
+            raise Exception("No SID found for domain '%s'" % domain)
 
-        #TODO: Get this information from the domain object
-        dsid = "S-1-5-21-328194278-237061239-1145748033"
+        dsid = sid[0]
+        #TODO: escape domain
+        ridbase = index.xquery("collection('objects')/o:SambaDomain[lower-case(*/o:sambaDomainName) = lower-case('%s')]//o:sambaAlgorithmicRidBase/string()" % domain)
+        if ridbase:
+            ridbase = int(ridbase[0])
+        else:
+            ridbase = int(self.env.config.get('samba.ridbase', default=1000))
 
         # Generate a sid for groups or users.
         group_type = int(group_type)
