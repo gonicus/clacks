4f0e2eaf0841fb30377fdd2c8c05fe0a05dfe80c
Fabian Hickert <hickert@gonicus.de>
2012-04-12 13:12:04 +0000

    Updated the partition type

--- agent/src/clacks/agent/plugins/goto/goto_types.py
+++ agent/src/clacks/agent/plugins/goto/goto_types.py
@@ -1,29 +1,38 @@
 # -*- coding: utf-8 -*-
 from clacks.agent.objects.types import AttributeType
 from clacks.common.components import PluginRegistry, ObjectRegistry
+from clacks.agent.jsonrpc_objects import JSONRPCObjectMapper
 
 
 class DevicePartitionTableType(AttributeType):
     """
-    TODO
+    A special attribute-type-definition used by the InstalledDevice object-extension.
+    It converts a partition definition from string into a json-object representation that
+    can then be passed through the json proxy to the user.
     """
 
     __alias__ = "DevicePartitionTableType"
 
     def values_match(self, value1, value2):
-        #TODO
         return(str(value1) == str(value2))
 
     def is_valid_value(self, value):
-        #TODO
-        return False
+        for item in value:
+            if type(item) != dict or '__jsonclass__' not in item:
+                return False
+        return True
 
     def _convert_to_unicodestring(self, value):
-        #TODO
-        return(value)
+        rom = PluginRegistry.getInstance('JSONRPCObjectMapper')
+        cr = PluginRegistry.getInstance('CommandRegistry')
+        new_value = []
+        for item in value:
+            uuid = item['__jsonclass__'][1][1]
+            new_value.append(rom.dispatchObjectMethod(uuid, 'dump'))
+            cr.call('closeObject', uuid)
+        return(new_value)
 
     def _convert_from_string(self, value):
-        #TODO
         return self._convert_from_unicodestring(value)
 
     def _convert_from_unicodestring(self, value):
