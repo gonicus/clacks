95aa8d5f1a7b6fb591f24f9e3b1a44f68dd8b040
Fabian Hickert <hickert@gonicus.de>
2012-05-25 13:24:31 +0000

    Updated AclAction-type to include acl-options and rolenames

--- agent/src/clacks/agent/objects/types/acl_actions.py
+++ agent/src/clacks/agent/objects/types/acl_actions.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from clacks.agent.objects.types import AttributeType
-
+from json import loads, dumps
 
 class AclAction(AttributeType):
     """
@@ -12,22 +12,36 @@ class AclAction(AttributeType):
     __alias__ = "AclAction"
 
     def values_match(self, value1, value2):
+        """
+        Checks whether the given values are equal
+        """
         return(str(value1) == str(value2))
 
     def is_valid_value(self, value):
+        """
+        Checks if the given value for AclAction is valid.
+        """
         if len(value):
             for entry in value:
+
+                # This object has to be of type dict
                 if type(entry) != dict:
                     return False
+
+                # We can either have a reference to a role or define permissions
+                # manually by specifying 'topic', 'acl' (and 'options')
+                if "rolename" in entry and len(entry) == 1:
+                    return True
+
+                # If acls were specified manually than check for the required
+                # parameters
                 if not "topic" in entry or "acl" not in entry:
                     return False
                 if type(entry["topic"]) not in  [str, unicode]:
                     return False
                 if type(entry["acl"]) not in [str, unicode]:
-                    print "4"
                     return False
                 if "options" in entry and type(entry["options"]) != dict:
-                    print "5"
                     return False
 
         return True
@@ -35,18 +49,41 @@ class AclAction(AttributeType):
     def _convert_to_unicodestring(self, value):
         """
         This method is a converter used when values gets read from or written to the backend.
-
         Converts the 'AclAction' object-type into a 'UnicodeString'-object.
         """
 
         if len(value):
+
+            # Walk through given Acl-objects and transform them into unicode
             res = []
             for entry in value:
-                res.append(entry["topic"] + ":" + entry["acl"])
+
+                # This acl-object uses a role instead of defining acls itself.
+                if "rolename" in entry:
+                    str_acl = "%s" % (entry["rolename"])
+
+                else:
+
+                    # This acl-object defines its own acls.
+                    # it will look like: <topic>:<acl>:<options>
+                    #              e.g.: ^net\.example\..*$:rwcdsex:user=horst,maildomain=gonicus.de
+                    str_acl = entry["topic"] + ":" + entry["acl"]
+                    opts = []
+                    if "options" in entry:
+                        for key, val in entry['options'].items():
+                            opts.append("%s=%s" % (key, val))
+
+                        if opts:
+                            str_acl += ":" + ",".join(opts)
+                # Append the generated result string
+                res.append(str_acl)
             return res
         return(value)
 
     def _convert_from_string(self, value):
+        """
+        See _convert_from_unicodestring
+        """
         return self._convert_from_unicodestring(value)
 
     def _convert_from_unicodestring(self, value):
@@ -55,14 +92,33 @@ class AclAction(AttributeType):
         Converts an 'UnicodeString' string into a 'AclAction'-object.
         """
         if len(value):
+
+            # Go through all attribute values and extract the informations
+            # An acl-entry looks like:  <topic>:<acl>:<options>
+            #         and a role like:  <rolename>
             res = []
             for entry in value:
-                print entry
-                data = entry.split(":")
+
+                # Split the given string by ':'
                 item = {}
-                item["topic"] = data[0]
-                item["acl"] = data[1]
-                item["options"] = {}
+                data = entry.split(":")
+
+                # This acl uses a role
+                if len(data) == 1:
+                    item["rolename"] = data[0]
+                else:
+
+                    # This acl defines its own acl, now extract topic, acls and maybe options too.
+                    item["topic"] = data[0]
+                    item["acl"] = data[1]
+
+                    # Extract options
+                    item["options"] = {}
+                    if len(data) == 3:
+                        opts = data[2].split(",")
+                        for opt in opts:
+                            key, value = opt.split("=")
+                            item["options"][key] = value
                 res.append(item)
             return res
         return(value)
