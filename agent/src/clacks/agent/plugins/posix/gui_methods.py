4f8e357444a99f22dbf89e9825940d87b8fb24ed
Fabian Hickert <hickert@gonicus.de>
2012-08-15 09:55:46 +0000

    Added getObjectDetails method

--- agent/src/clacks/agent/plugins/posix/gui_methods.py
+++ agent/src/clacks/agent/plugins/posix/gui_methods.py
@@ -10,12 +10,43 @@ class GuiMethods(Plugin):
     _target_ = 'misc'
 
     @Command(__help__=N_("Resolves object information"))
-    def getObjectDetails(self, cn, attributes):
+    def getObjectDetails(self, names, attributes):
         """
         """
-        index = PluginRegistry.getInstance("ObjectIndex")
-        print index.xquery_dict("collection('objects')/o:PosixGroup/o:Attributes[o:cn='%s']" % (cn))
 
-        return None
+        otype = "PosixGroup"
+        oattr = "cn"
+        base = "dc=example,dc=net"
+
+        l = []
+        for item in names:
+            l.append('(%s.%s = "%s")' % (otype, oattr, item))
+
+        a = []
+        for attr in attributes:
+            a.append("%s.%s" % (otype, attr))
+
+        attrs = ", ".join(a)
+        condition = " OR ".join(l)
+
+
+        query = """
+            SELECT %(attrs)s
+            BASE %(type)s SUB "%(base)s"
+            WHERE %(condition)s
+            """ % {"attrs": attrs, "condition": condition, "type": otype, "base": base}
+
+        search = PluginRegistry.getInstance("SearchWrapper")
+        res =  search.execute(query)
+        result = []
+        for entry in res:
+            item = {}
+            for attr in attributes:
+                if attr in entry[otype] and len(entry[otype][attr]):
+                    item[attr] = entry[otype][attr][0]
+                else:
+                    item[attr] = ""
+            result.append(item)
+        return result
 
 
