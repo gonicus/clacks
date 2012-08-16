7e08a018dc675b95153b4832be3c7116bcc8bba7
cajus <pollmeier@gonicus.de>
2012-08-16 11:29:28 +0200

    Fixed shadow values

--- agent/src/clacks/agent/data/templates/default/i18n/posix-user_de.ts
+++ agent/src/clacks/agent/data/templates/default/i18n/posix-user_de.ts
@@ -49,9 +49,13 @@
         <translation>Automatischer GECOS</translation>
     </message>
     <message>
-        <location filename="../posix-user.ui" line="182"/>
         <source>Members</source>
-        <translation>Mitglieder</translation>
+        <translation type="obsolete">Mitglieder</translation>
+    </message>
+    <message>
+        <location filename="../posix-user.ui" line="182"/>
+        <source>Group membership</source>
+        <translation>Gruppenmitgliedschaft</translation>
     </message>
     <message>
         <location filename="../posix-user.ui" line="202"/>
@@ -77,33 +81,52 @@
     </message>
     <message>
         <location filename="../posix-user.ui" line="248"/>
-        <source>shadowMin</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <location filename="../posix-user.ui" line="261"/>
-        <source>shadowMax</source>
-        <translation type="unfinished"></translation>
+        <source>Specifies the minimum number of days required between password changes.</source>
+        <translation>Gibt die minimale Anzahl von Tagen an, die zwischen Passwortänderungen vergehen müssen.</translation>
     </message>
     <message>
-        <location filename="../posix-user.ui" line="310"/>
-        <source>shadowWarning</source>
-        <translation type="unfinished"></translation>
+        <location filename="../posix-user.ui" line="251"/>
+        <source>Minimum password age</source>
+        <translation>Minimales Passwort-Alter</translation>
     </message>
     <message>
-        <location filename="../posix-user.ui" line="323"/>
-        <source>shadowInactive</source>
-        <translation type="unfinished"></translation>
+        <location filename="../posix-user.ui" line="268"/>
+        <location filename="../posix-user.ui" line="281"/>
+        <source>Specifies the maximum number of days the password is valid.</source>
+        <translation>Gibt an wie viele Tage ein Passwort maximal gültig ist.</translation>
     </message>
     <message>
-        <location filename="../posix-user.ui" line="336"/>
+        <location filename="../posix-user.ui" line="271"/>
+        <source>Maximum password age</source>
+        <translation>Maximales Passwort-Alter</translation>
+    </message>
+    <message>
+        <location filename="../posix-user.ui" line="327"/>
+        <location filename="../posix-user.ui" line="340"/>
+        <source>Specifies the number of days before the password expires that the user is warned.</source>
+        <translation>Gibt die Anzahl der Tage an, vor denen ein Benutzer vor dem Ablauf seines Kennwortes gewarnt werden soll.</translation>
+    </message>
+    <message>
+        <location filename="../posix-user.ui" line="330"/>
+        <source>Warn before password expires</source>
+        <translation>Warnen vor Ablauf des Passwortes</translation>
+    </message>
+    <message>
+        <location filename="../posix-user.ui" line="347"/>
+        <location filename="../posix-user.ui" line="360"/>
+        <source>Specifies the number of days of inactivity allowed for the specified user.</source>
+        <translation>Gibt die Anzahl der Tage an, die ein Benutzer inaktiv sein darf.</translation>
+    </message>
+    <message>
+        <location filename="../posix-user.ui" line="350"/>
+        <source>Inactivity before account is disabled</source>
+        <translation>Inaktivität bevor das Konto deaktiviert wird</translation>
+    </message>
+    <message>
+        <location filename="../posix-user.ui" line="367"/>
         <source>Password expires on</source>
         <translation>Passwort läuft ab am</translation>
-    </message>
-    <message>
-        <location filename="../posix-user.ui" line="349"/>
-        <source>shadowFlag</source>
-        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
