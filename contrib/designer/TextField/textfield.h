9a56953c7fc849d0e9efcf939193a1b6cf4e8d17
cajus <pollmeier@gonicus.de>
2012-08-20 14:25:04 +0200

    Added more images

--- contrib/designer/TextField/textfield.h
+++ contrib/designer/TextField/textfield.h
@@ -7,9 +7,26 @@
 class TextField : public QLineEdit
 {
     Q_OBJECT
-    
-public:
+    Q_PROPERTY(QList<QString> Actions READ getActions WRITE setActions)
+    Q_PROPERTY(int test READ getTest WRITE setTest)
+
+  public:
     TextField(QWidget *parent = 0);
+    QList<QString> getActions () const;
+    int getTest () const;
+
+  public slots:
+    void setActions (const QList<QString> &actions);
+    void setTest (const int test);
+
+  private:
+    QList<QString> actions;
+    int test;
+
+  signals:
+    void actionsChanged(const QList<QString> &actions);
+    void testChanged(const int test);
+
 };
 
 #endif
