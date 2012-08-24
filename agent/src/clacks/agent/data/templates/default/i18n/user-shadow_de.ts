8d4ef6835c95561286c73ee71cc8f76258131a9d
Cajus Pollmeier <pollmeier@gonicus.de>
2012-08-24 07:46:46 +0200

    Updated trn

--- agent/src/clacks/agent/data/templates/default/i18n/user-shadow_de.ts
+++ agent/src/clacks/agent/data/templates/default/i18n/user-shadow_de.ts
@@ -6,71 +6,71 @@
     <message>
         <location filename="../user-shadow.ui" line="14"/>
         <source>Shadow</source>
-        <translation type="unfinished"></translation>
+        <translation>Shadow</translation>
     </message>
     <message>
         <location filename="../user-shadow.ui" line="24"/>
         <source>Password expiry</source>
-        <translation type="unfinished"></translation>
+        <translation>Passwort-Ablauf</translation>
     </message>
     <message>
         <location filename="../user-shadow.ui" line="32"/>
         <source>Last password modification</source>
-        <translation type="unfinished">Letzte Passwortänderung</translation>
+        <translation>Letzte Passwortänderung</translation>
     </message>
     <message>
         <location filename="../user-shadow.ui" line="61"/>
         <source>Password expires on</source>
-        <translation type="unfinished">Passwort läuft ab am</translation>
+        <translation>Passwort läuft ab am</translation>
     </message>
     <message>
         <location filename="../user-shadow.ui" line="74"/>
         <location filename="../user-shadow.ui" line="87"/>
         <source>Specifies the number of days before the password expires that the user is warned.</source>
-        <translation type="unfinished">Gibt die Anzahl der Tage an, vor denen ein Benutzer vor dem Ablauf seines Kennwortes gewarnt werden soll.</translation>
+        <translation>Gibt die Anzahl der Tage an, vor denen ein Benutzer vor dem Ablauf seines Kennwortes gewarnt werden soll.</translation>
     </message>
     <message>
         <location filename="../user-shadow.ui" line="77"/>
         <source>Warn before password expires</source>
-        <translation type="unfinished">Warnen vor Ablauf des Passwortes</translation>
+        <translation>Warnen vor Ablauf des Passwortes</translation>
     </message>
     <message>
         <location filename="../user-shadow.ui" line="94"/>
         <location filename="../user-shadow.ui" line="107"/>
         <source>Specifies the number of days of inactivity allowed for the specified user.</source>
-        <translation type="unfinished">Gibt die Anzahl der Tage an, die ein Benutzer inaktiv sein darf.</translation>
+        <translation>Gibt die Anzahl der Tage an, die ein Benutzer inaktiv sein darf.</translation>
     </message>
     <message>
         <location filename="../user-shadow.ui" line="97"/>
         <source>Inactivity before account is disabled</source>
-        <translation type="unfinished">Inaktivität bevor das Konto deaktiviert wird</translation>
+        <translation>Inaktivität bevor das Konto deaktiviert wird</translation>
     </message>
     <message>
         <location filename="../user-shadow.ui" line="119"/>
         <source>Options</source>
-        <translation type="unfinished"></translation>
+        <translation>Optionen</translation>
     </message>
     <message>
         <location filename="../user-shadow.ui" line="127"/>
         <location filename="../user-shadow.ui" line="140"/>
         <source>Specifies the minimum number of days required between password changes.</source>
-        <translation type="unfinished">Gibt die minimale Anzahl von Tagen an, die zwischen Passwortänderungen vergehen müssen.</translation>
+        <translation>Gibt die minimale Anzahl von Tagen an, die zwischen Passwortänderungen vergehen müssen.</translation>
     </message>
     <message>
         <location filename="../user-shadow.ui" line="130"/>
         <source>Minimum password age</source>
-        <translation type="unfinished">Minimales Passwort-Alter</translation>
+        <translation>Minimales Passwort-Alter</translation>
     </message>
     <message>
         <location filename="../user-shadow.ui" line="147"/>
         <location filename="../user-shadow.ui" line="160"/>
         <source>Specifies the maximum number of days the password is valid.</source>
-        <translation type="unfinished">Gibt an wie viele Tage ein Passwort maximal gültig ist.</translation>
+        <translation>Gibt an wie viele Tage ein Passwort maximal gültig ist.</translation>
     </message>
     <message>
         <location filename="../user-shadow.ui" line="150"/>
         <source>Maximum password age</source>
-        <translation type="unfinished">Maximales Passwort-Alter</translation>
+        <translation>Maximales Passwort-Alter</translation>
     </message>
 </context>
 </TS>
