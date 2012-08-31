0423cc00d4f8338354aee50e73914b34c0018cf6
Fabian Hickert <hickert@gonicus.de>
2012-08-31 07:36:03 +0000

    Updated getSambaDomainNames method to return real values

--- agent/src/clacks/agent/plugins/samba/gui_methods.py
+++ agent/src/clacks/agent/plugins/samba/gui_methods.py
@@ -2,11 +2,13 @@
 from clacks.common.components import Command
 from clacks.common.components import Plugin
 from clacks.common.utils import N_
+from clacks.common.components import PluginRegistry
 
 
 class SambaGuiMethods(Plugin):
     _target_ = 'misc'
 
-    @Command(__help__=N_("Returns a list of all selectable samba domain names"))
+    @Command(__help__=N_("Returns a list with all selectable samba-domain-names"))
     def getSambaDomainNames(self):
-        return ["a", "b"]
+        index = PluginRegistry.getInstance("ObjectIndex")
+        return index.xquery("collection('objects')/o:SambaDomain/o:Attributes/o:sambaDomainName/string()")
