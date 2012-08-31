fd9061d972b61f9429f28e077de484d3d858b0f6
cajus <pollmeier@gonicus.de>
2012-08-31 09:36:57 +0200

    Added new ts

---
+++ agent/src/clacks/agent/data/templates/default/i18n/user-samba_de.ts
@@ -0,0 +1,125 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE">
+<context>
+    <name>sambaSamAccount</name>
+    <message>
+        <location filename="../user-samba.ui" line="14"/>
+        <source>Samba</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../user-samba.ui" line="24"/>
+        <source>Common</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../user-samba.ui" line="34"/>
+        <source>SID</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../user-samba.ui" line="47"/>
+        <source>Domain</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../user-samba.ui" line="60"/>
+        <source>Primary group</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../user-samba.ui" line="93"/>
+        <location filename="../user-samba.ui" line="360"/>
+        <source>Description</source>
+        <extracomment>description</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../user-samba.ui" line="106"/>
+        <source>Automatic display name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../user-samba.ui" line="113"/>
+        <source>Display name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../user-samba.ui" line="144"/>
+        <source>Home directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../user-samba.ui" line="171"/>
+        <source>Logon script</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../user-samba.ui" line="184"/>
+        <source>Profile path</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../user-samba.ui" line="204"/>
+        <source>Password options</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../user-samba.ui" line="214"/>
+        <source>sambaPwdLastSet</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../user-samba.ui" line="227"/>
+        <source>sambaLogonTime</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../user-samba.ui" line="240"/>
+        <source>sambaLogoffTime</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../user-samba.ui" line="253"/>
+        <source>sambaBadPasswordCount</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../user-samba.ui" line="286"/>
+        <source>sambaKickoffTime</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../user-samba.ui" line="299"/>
+        <source>sambaPwdCanChange</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../user-samba.ui" line="312"/>
+        <source>sambaPwdMustChange</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../user-samba.ui" line="328"/>
+        <source>sambaBadPasswordTime</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../user-samba.ui" line="345"/>
+        <source>Allow login for these workstations only</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../user-samba.ui" line="351"/>
+        <source>Edit list of workstations</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../user-samba.ui" line="355"/>
+        <source>Name</source>
+        <extracomment>cn</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
