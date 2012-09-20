781bc11965b78edaf40a6428711e5220b0a4f565
Cajus Pollmeier <pollmeier@gonicus.de>
2012-09-20 11:34:08 +0200

    Added group tab initially

---
+++ agent/src/clacks/agent/data/templates/default/i18n/group-posix_de.ts
@@ -0,0 +1,70 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE">
+<context>
+    <name>posixGroup</name>
+    <message>
+        <location filename="../group-posix.ui" line="14"/>
+        <source>POSIX</source>
+        <translation>POSIX</translation>
+    </message>
+    <message>
+        <location filename="../group-posix.ui" line="17"/>
+        <source>Group</source>
+        <translation>Gruppe</translation>
+    </message>
+    <message>
+        <location filename="../group-posix.ui" line="24"/>
+        <source>Edit members</source>
+        <translation>Mitglieder bearbeiten</translation>
+    </message>
+    <message>
+        <location filename="../group-posix.ui" line="30"/>
+        <source>Generic</source>
+        <translation>Allgmein</translation>
+    </message>
+    <message>
+        <location filename="../group-posix.ui" line="38"/>
+        <source>Group name</source>
+        <translation>Name</translation>
+    </message>
+    <message>
+        <location filename="../group-posix.ui" line="51"/>
+        <source>Description</source>
+        <translation>Beschreibung</translation>
+    </message>
+    <message>
+        <location filename="../group-posix.ui" line="64"/>
+        <source>Automatically assign group ID</source>
+        <translation>Gruppen-ID automatisch vergeben</translation>
+    </message>
+    <message>
+        <location filename="../group-posix.ui" line="71"/>
+        <source>Group ID</source>
+        <translation>Gruppen-ID</translation>
+    </message>
+    <message>
+        <location filename="../group-posix.ui" line="105"/>
+        <source>Members</source>
+        <translation>Mitglieder</translation>
+    </message>
+    <message>
+        <location filename="../group-posix.ui" line="112"/>
+        <source>Name</source>
+        <comment>sn</comment>
+        <translation>Name</translation>
+    </message>
+    <message>
+        <location filename="../group-posix.ui" line="117"/>
+        <source>Given name</source>
+        <comment>givenName</comment>
+        <translation>Vorname</translation>
+    </message>
+    <message>
+        <location filename="../group-posix.ui" line="122"/>
+        <source>Login ID</source>
+        <comment>uid</comment>
+        <translation>Anmelde-ID</translation>
+    </message>
+</context>
+</TS>
