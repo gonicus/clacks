2d066529fd1ecbe5b48f0bf1bd7c5f62ea2a2f99
Cajus Pollmeier <pollmeier@gonicus.de>
2012-09-20 12:31:48 +0200

    Updated samba group ui

---
+++ agent/src/clacks/agent/data/templates/default/i18n/group-samba_de.ts
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE">
+<context>
+    <name>sambaGroupMapping</name>
+    <message>
+        <location filename="../group-samba.ui" line="14"/>
+        <source>Samba</source>
+        <translation>Samba</translation>
+    </message>
+    <message>
+        <location filename="../group-samba.ui" line="24"/>
+        <source>Generic</source>
+        <translation>Allgemein</translation>
+    </message>
+    <message>
+        <location filename="../group-samba.ui" line="30"/>
+        <source>Group type</source>
+        <translation>Gruppen-Typ</translation>
+    </message>
+    <message>
+        <location filename="../group-samba.ui" line="40"/>
+        <source>Display name</source>
+        <translation>Angezeigter Name</translation>
+    </message>
+    <message>
+        <location filename="../group-samba.ui" line="58"/>
+        <source>Nested groups</source>
+        <translation>Verschachtelte Gruppen</translation>
+    </message>
+    <message>
+        <location filename="../group-samba.ui" line="64"/>
+        <source>Edit nested groups</source>
+        <translation>Verschachtelte Gruppen bearbeiten</translation>
+    </message>
+    <message>
+        <location filename="../group-samba.ui" line="68"/>
+        <source>Name</source>
+        <extracomment>cn</extracomment>
+        <translation>Name</translation>
+    </message>
+    <message>
+        <location filename="../group-samba.ui" line="73"/>
+        <source>Description</source>
+        <extracomment>description</extracomment>
+        <translation>Beschreibung</translation>
+    </message>
+    <message>
+        <location filename="../group-samba.ui" line="78"/>
+        <source>SID</source>
+        <extracomment>sambaSID</extracomment>
+        <translation>SID</translation>
+    </message>
+</context>
+</TS>
