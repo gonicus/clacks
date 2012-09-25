b805a881edfc17e7e1176bacfa9f864a39b1d8c0
Fabian Hickert <hickert@gonicus.de>
2012-09-25 09:55:54 +0000

    Added GOsa Account definitions

---
+++ contrib/GOsaAccount/data/templates/default/i18n/user-posix_de.ts
@@ -0,0 +1,74 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE">
+<context>
+    <name>posixAccount</name>
+    <message>
+        <location filename="../user-posix.ui" line="14"/>
+        <source>POSIX</source>
+        <translation>POSIX</translation>
+    </message>
+    <message>
+        <location filename="../user-posix.ui" line="24"/>
+        <source>Generic POSIX settings</source>
+        <translation>Allgemeine POSIX-Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="../user-posix.ui" line="35"/>
+        <source>Home directory</source>
+        <translation>Benutzerverzeichnis</translation>
+    </message>
+    <message>
+        <location filename="../user-posix.ui" line="48"/>
+        <source>Login shell</source>
+        <translation>Anmelde-Interpreter</translation>
+    </message>
+    <message>
+        <location filename="../user-posix.ui" line="61"/>
+        <source>GECOS</source>
+        <translation>GECOS</translation>
+    </message>
+    <message>
+        <location filename="../user-posix.ui" line="90"/>
+        <source>Automatic UID/GID numbers</source>
+        <translation>Automatische UID-/GID-Nummern</translation>
+    </message>
+    <message>
+        <location filename="../user-posix.ui" line="97"/>
+        <source>User ID</source>
+        <translation>Benutzer-ID</translation>
+    </message>
+    <message>
+        <location filename="../user-posix.ui" line="117"/>
+        <source>Group ID</source>
+        <translation>Gruppen-ID</translation>
+    </message>
+    <message>
+        <location filename="../user-posix.ui" line="137"/>
+        <source>Automatic GECOS</source>
+        <translation>Automatischer GECOS</translation>
+    </message>
+    <message>
+        <location filename="../user-posix.ui" line="165"/>
+        <source>Group membership</source>
+        <translation>Gruppenmitgliedschaft</translation>
+    </message>
+    <message>
+        <location filename="../user-posix.ui" line="183"/>
+        <source>Edit group membership</source>
+        <translation>Gruppenmitgliedschaft bearbeiten</translation>
+    </message>
+    <message>
+        <location filename="../user-posix.ui" line="187"/>
+        <source>Name</source>
+        <comment>cn</comment>
+        <translation>Name</translation>
+    </message>
+    <message>
+        <location filename="../user-posix.ui" line="192"/>
+        <source>Description</source>
+        <comment>description</comment>
+        <translation>Beschreibung</translation>
+    </message>
+</context>
+</TS>
---
+++ contrib/GOsaAccount/data/templates/default/i18n/user_de.ts
@@ -0,0 +1,122 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE">
+<context>
+    <name>User</name>
+    <message>
+        <location filename="../user.ui" line="14"/>
+        <source>Personal</source>
+        <translation>Persönlich</translation>
+    </message>
+    <message>
+        <location filename="../user.ui" line="21"/>
+        <source>User</source>
+        <translation>Benutzer</translation>
+    </message>
+    <message>
+        <location filename="../user.ui" line="27"/>
+        <source>Login information</source>
+        <translation>Anmelde-Information</translation>
+    </message>
+    <message>
+        <location filename="../user.ui" line="76"/>
+        <source>Login &amp;ID</source>
+        <translation>Anmelde &amp;ID</translation>
+    </message>
+    <message>
+        <location filename="../user.ui" line="105"/>
+        <source>Surname</source>
+        <translation>Nachname</translation>
+    </message>
+    <message>
+        <location filename="../user.ui" line="118"/>
+        <source>Given name</source>
+        <translation>Vorname</translation>
+    </message>
+    <message>
+        <location filename="../user.ui" line="147"/>
+        <source>Preferred language</source>
+        <translation>Bevorzugte Sprache</translation>
+    </message>
+    <message>
+        <location filename="../user.ui" line="178"/>
+        <source>Contact information</source>
+        <translation>Kontakt-Information</translation>
+    </message>
+    <message>
+        <location filename="../user.ui" line="188"/>
+        <source>Automatic display name</source>
+        <translation>Automatischer angezeigter Name</translation>
+    </message>
+    <message>
+        <location filename="../user.ui" line="195"/>
+        <source>Display name</source>
+        <translation>Angezeigter Name</translation>
+    </message>
+    <message>
+        <location filename="../user.ui" line="208"/>
+        <source>Title</source>
+        <translation>Titel</translation>
+    </message>
+    <message>
+        <location filename="../user.ui" line="221"/>
+        <source>Academic title</source>
+        <translation>Akademischer Titel</translation>
+    </message>
+    <message>
+        <location filename="../user.ui" line="250"/>
+        <source>Date of birth</source>
+        <translation>Geburtsdatum</translation>
+    </message>
+    <message>
+        <location filename="../user.ui" line="279"/>
+        <source>Gender</source>
+        <translation>Geschlecht</translation>
+    </message>
+    <message>
+        <location filename="../user.ui" line="312"/>
+        <source>Address</source>
+        <translation>Adresse</translation>
+    </message>
+    <message>
+        <location filename="../user.ui" line="344"/>
+        <source>Phone</source>
+        <translation>Telefon</translation>
+    </message>
+    <message>
+        <location filename="../user.ui" line="357"/>
+        <source>Homepage</source>
+        <translation>Internetseite</translation>
+    </message>
+    <message>
+        <location filename="../user.ui" line="367"/>
+        <source>Please provide your web page here</source>
+        <translation>Bitte geben Sie hier ihre Webseite ein</translation>
+    </message>
+    <message>
+        <location filename="../user.ui" line="385"/>
+        <source>Change password...</source>
+        <translation>Passwort ändern...</translation>
+    </message>
+    <message>
+        <location filename="../user.ui" line="388"/>
+        <source>Ctrl+P</source>
+        <translation>Ctrl+P</translation>
+    </message>
+    <message>
+        <location filename="../user.ui" line="403"/>
+        <source>Lock account</source>
+        <translation>Konto sperren</translation>
+    </message>
+    <message>
+        <location filename="../user.ui" line="418"/>
+        <source>Unlock account</source>
+        <translation>Konto freigeben</translation>
+    </message>
+    <message>
+        <location filename="../user.ui" line="433"/>
+        <source>Send message...</source>
+        <translation>Nachricht senden...</translation>
+    </message>
+</context>
+</TS>
