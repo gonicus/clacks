24b1fe104559fd813e1574c5d1e081424d7eaea0
Cajus Pollmeier <pollmeier@gonicus.de>
2012-10-05 12:12:49 +0200

    Updated license stuff

--- plugins/amires/src/amires/modules/doingreport_render.py
+++ plugins/amires/src/amires/modules/doingreport_render.py
@@ -1,4 +1,15 @@
-# -*- coding: utf-8 -*-
+# This file is part of the clacks framework.
+#
+#  http://clacks-project.org
+#
+# Copyright:
+#  (C) 2010-2012 GONICUS GmbH, Germany, http://www.gonicus.de
+#
+# License:
+#  GPL-2: http://www.gnu.org/licenses/gpl-2.0.html
+#
+# See the LICENSE file in the project's top-level directory for details.
+
 import pkg_resources
 import gettext
 import math
