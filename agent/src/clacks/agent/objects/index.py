d0b22005e6929568217ea21443f4b9e2824518ec
Cajus Pollmeier <pollmeier@gonicus.de>
2012-10-16 12:01:23 +0200

    Don't handle retract/extend separately - they're allways triggered thru object proxies "update"

--- agent/src/clacks/agent/objects/index.py
+++ agent/src/clacks/agent/objects/index.py
@@ -443,16 +443,6 @@ class ObjectIndex(Plugin):
                 _dn = obj.dn
                 change_type = "create"
 
-            if event.reason in ["post retract", "post extend"]:
-                self.log.debug("updating object index for %s" % _uuid)
-                if not event.dn:
-                    entry = self.db.index.find_one({'_uuid': _uuid, 'dn': {'$exists': 1}}, {'dn': 1})
-                    if entry:
-                        event.dn = entry['dn']
-
-                obj = ObjectProxy(event.dn)
-                self.update(obj)
-
             if event.reason in ["post object update"]:
                 self.log.debug("updating object index for %s" % _uuid)
                 if not event.dn:
