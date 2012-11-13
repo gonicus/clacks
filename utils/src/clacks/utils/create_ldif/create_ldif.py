9217e567353541d7e99c90bcdf333d61cdf933f7
Fabian Hickert <hickert@gonicus.de>
2012-11-13 12:56:51 +0000

    Updated ldap generator script

--- utils/src/clacks/utils/create_ldif/create_ldif.py
+++ utils/src/clacks/utils/create_ldif/create_ldif.py
@@ -1,16 +1,110 @@
 #!/usr/bin/env python
 import argparse
 import os
+import re
 import sys
 
 class NoSuchTemplateException(Exception):
     pass
+
+
+class Line(object):
+
+    generator = None
+    attrName = None
+    elements = None
+
+    def __init__(self, generator, attrName, elements):
+
+        self.generator = generator
+        self.attrName = attrName
+        self.elements = elements
+
+    def __repr__(self):
+        return self.attrName + ": ..."
+
+    def process(self):
+        result = ""
+        for item in self.elements:
+            if type(item) == str:
+                result = result + item
+            else:
+                result = result + item.process()
+
+        return result
+
+
+class getFunction(object):
+
+    name = None
+    generator= None
+    params = None
+
+    def __init__(self, generator, parts):
+
+        name = re.sub("^([^(]*).*$", "\\1", "".join([f for f in parts[1:-1] if type(f) in [str]]))
+        tmp_params = parts[len(name)+2:-2]
+        params = {}
+
+        param_id = 0
+        params[param_id] = []
+        for item in tmp_params:
+            if item == ",":
+                param_id = param_id + 1 
+                params[param_id] = []
+                continue
+
+            params[param_id].append(item)
+
+        self.params = params.values()
+        self.name = name
+        self.generator = generator
+
+    def __repr__(self):
+        return self.name + "(...)"
+
+    def process(self):
+
+        func = getattr(self.generator, self.name)
+        params = []
+        for para in self.params:
+            str_para = ""
+            for item in para:
+                if type(item) == str:
+                    str_para = str_para + item
+                else:
+                    str_para = str_para + item.process()
+
+            params.append(str_para)
+        return func(params)
+
+
+class getAttr(object):
+
+    name = None
+    generator= None
+
+    def __init__(self, generator, name):
+        self.name = name
+        self.generator = generator
+
+    def __repr__(self):
+        return "["+self.name+"]"
+
+    def process(self):
+        return self.name
 
 class LdifGenerator(object):
 
     _use = {}
     _templates = {}
     _templatePath = None
+
+    def generate_unique_dn(self, args):
+        return "called"
+
+    def generate_unique_uid(self, args):
+        return "called"
 
     def __init__(self, templatePath):
         self._templatePath = templatePath
@@ -31,26 +125,53 @@ class LdifGenerator(object):
         The templates will be stored in self._templates.
         """
         fileList = []
-        rootdir = sys.argv[1]
         for root, subFolders, files in os.walk(self._templatePath):
             for file in files:
                 path = os.path.join(root, file)
-                self._templates[os.path.splitext(file)[0]] = open(path).read();
+                self._templates[os.path.splitext(file)[0]] = self._processTemplate(open(path).read());
 
-    def _processTemplate(self, o_type):
+    def _processTemplate(self, content):
 
-        lines = self._templates[o_type].split("\n")
+        lines = content.split("\n")
+        objectList = []
+        regex = "(^.*(%([a-zA-Z_][a-zA-Z0-9_-]*)?\([^\)\(]*\)[fs]).*)$"
         for line in lines:
-            print line
+
+            attrName = re.sub("^([^=]*)=.*$", "\\1" , line)
+            line = re.sub("^[^=]*=(.*)$", "\\1" , line)
+            match = re.match(regex, line)
+
+            lineList = list(line)
+
+            while match:
+                line = match.group(0)
+                matched = match.group(2)
+                start = match.start(2)
+                end = match.end(2)
+
+                # Replace item with a string
+                if matched[-1] == "s":
+                    replacement = getAttr(self, matched[2:-2])
+
+                if matched[-1] == "f":
+                    replacement = getFunction(self, lineList[start:end])
+
+                line = line[0:start] + "!" +line[end:]
+                lineList = lineList[0:start] + [replacement] + lineList[end:]
+                match = re.match(regex, line)
+
+            line_object = Line(self, attrName, lineList)
+            objectList.append(line_object)
+        return(objectList)
 
 
     def generate(self):
 
-        for item in self._use:
-            amount = self._use[item]
+        for template in self._templates:
 
-            for i in range(amount):
-                self._processTemplate(item)
+            print "##" + template+ "##"
+            for line in self._templates[template]:
+                print line.process()
 
 
 def main():
